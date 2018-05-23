425739b1987f8a8a95baa736b36cd2d2cb3ef0ce
Graham Higgins <gjhiggins@users.noreply.github.com>
2018-05-23 19:51:32 +0100

    Update chainparamsbase.cpp
    change to use Helium-specific bindings

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -23,7 +23,7 @@ public:
     CBaseMainParams()
     {
         networkID = CBaseChainParams::MAIN;
-        nRPCPort = 51473;
+        nRPCPort = 9019;
     }
 };
 static CBaseMainParams mainParams;
@@ -37,7 +37,7 @@ public:
     CBaseTestNetParams()
     {
         networkID = CBaseChainParams::TESTNET;
-        nRPCPort = 51475;
+        nRPCPort = 19019;
         strDataDir = "testnet4";
     }
 };
