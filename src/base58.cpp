1b84e0ab749357e590f68a73b9778373711f8ef9
faetos <faetos@yahoo.com>
2018-06-19 11:35:20 -0500

    Made changes to support Mac build

--- src/base58.cpp
+++ src/base58.cpp
@@ -16,6 +16,7 @@
 #include <string.h>
 #include <string>
 #include <vector>
+#include <iostream>
 
 /** All alphanumeric characters except for "0", "I", "O", and "l" */
 static const char* pszBase58 = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
