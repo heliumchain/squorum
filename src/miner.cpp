a335ea1582343dd77f7692f96927037264119980
Fuzzbawls <fuzzbawls@gmail.com>
2018-06-23 03:32:06 -0700

    prevent infinate loop in miner

--- src/miner.cpp
+++ src/miner.cpp
@@ -684,7 +684,6 @@ void BitcoinMiner(CWallet* pwallet, bool fProofOfStake)
                     continue;
                 }
             }
-            fLastLoopOrphan = false;
         }
 
         //
