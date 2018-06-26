e1f214a07129fbed45690dd38f9b2f03b45396ac
faetos <faetos@yahoo.com>
2018-06-26 10:00:48 -0500

    Removed testnet changes for trial

--- src/main.cpp
+++ src/main.cpp
@@ -1816,15 +1816,34 @@ double ConvertBitsToDouble(unsigned int nBits)
 int64_t GetBlockValue(int nHeight)
 {
     if (Params().NetworkID() == CBaseChainParams::TESTNET) {
-        /* FIXME: GJH Set testnet PoW period reward */
-        if (nHeight < 10000 && nHeight > 0)
-            return 25 * COIN;
-    }
-
-    /* FIXME: GJH Particularise block reward scheme */
     int64_t nSubsidy = 0;
-    if (nHeight <= Params().LAST_POW_BLOCK()) {
-        nSubsidy = 10000 * COIN;
+    if (nHeight == 0) {
+        // Mint the ledger total (minus treasury deposit) for disbursal
+        nSubsidy = (ledgerTotal - treasuryDeposit); // (8891432 * COIN) - (432870.87949961 * COIN)
+    } else if (nHeight < 86400 && nHeight > 0) {
+        nSubsidy = 250 * COIN;
+    } else if (nHeight < (Params().NetworkID() == CBaseChainParams::TESTNET ? 145000 : 151200) && nHeight >= 86400) {
+        nSubsidy = 225 * COIN;
+    } else if (nHeight <= Params().LAST_POW_BLOCK() && nHeight >= 151200) {
+        nSubsidy = 45 * COIN;
+    } else if (nHeight <= 302399 && nHeight > Params().LAST_POW_BLOCK()) {
+        nSubsidy = 45 * COIN;
+    } else if (nHeight <= 345599 && nHeight >= 302400) {
+        nSubsidy = static_cast<int64_t>(40.5 * COIN);
+    } else if (nHeight <= 388799 && nHeight >= 345600) {
+        nSubsidy = 36 * COIN;
+    } else if (nHeight <= 431999 && nHeight >= 388800) {
+        nSubsidy = static_cast<int64_t>(31.5 * COIN);
+    } else if (nHeight <= 475199 && nHeight >= 432000) {
+        nSubsidy = 27 * COIN;
+    } else if (nHeight <= 518399 && nHeight >= 475200) {
+        nSubsidy = static_cast<int64_t>(22.5 * COIN);
+    } else if (nHeight <= 561599 && nHeight >= 518400) {
+        nSubsidy = 18 * COIN;
+    } else if (nHeight <= 604799 && nHeight >= 561600) {
+        nSubsidy = static_cast<int64_t>(13.5 * COIN);
+    } else if (nHeight <= 647999 && nHeight >= 604800) {
+        nSubsidy = 9 * COIN;
     } else if (nHeight < Params().Zerocoin_Block_V2_Start()) {
         nSubsidy = 4.5 * COIN;
     } else {
@@ -2080,8 +2099,9 @@ int64_t GetMasternodePayment(int nHeight, int64_t blockValue, int nMasternodeCou
     }
 
     /* FIXME: GJH Temporary hack to investigate initial PoW period */
-    if (nHeight <= 10000)
-        ret = 0;
+    // NH - commented out //
+    //if (nHeight <= 10000)
+    //    ret = 0;
 
     /* FIXME: GJH Particularise masternode payment schedule */
     if (nHeight <= 43200) {
