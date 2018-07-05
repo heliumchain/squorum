fc52bdde8f2f51a650c6a2de396005eb05ae73e2
faetos <faetos@yahoo.com>
2018-07-05 12:42:04 -0500

    made changes to testnet seeds - NH

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -2385,7 +2385,7 @@ public:
         }
 
 
-        vSeeds.push_back(CDNSSeedData("Minkiz", "minkiz.co"));           // GJH Node
+        //vSeeds.push_back(CDNSSeedData("Minkiz", "minkiz.co"));           // GJH Node
         vSeeds.push_back(CDNSSeedData("heliumna01", "heliumna01.heliumchain.org")); 
         vSeeds.push_back(CDNSSeedData("heliumeu01", "heliumeu01.heliumchain.org"));
         vSeeds.push_back(CDNSSeedData("heliumap01", "heliumap01.heliumchain.org"));
@@ -2531,7 +2531,10 @@ public:
 
         vFixedSeeds.clear();
         vSeeds.clear();
-        vSeeds.push_back(CDNSSeedData("Minkiz", "minkiz.co"));
+        //vSeeds.push_back(CDNSSeedData("Minkiz", "minkiz.co"));
+        vSeeds.push_back(CDNSSeedData("heliumna01", "heliumna01.heliumchain.org"));
+        vSeeds.push_back(CDNSSeedData("heliumeu01", "heliumeu01.heliumchain.org"));
+        vSeeds.push_back(CDNSSeedData("heliumap01", "heliumap01.heliumchain.org"));
 
         // Testnet Helium addresses start with 'm' or 'n'
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
