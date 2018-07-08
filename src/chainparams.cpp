835a2c579eea0cad01b06495a6e5d5896cfd1998
faetos <faetos@yahoo.com>
2018-07-07 23:49:32 -0500

    fixed error

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -2621,7 +2621,7 @@ public:
                     0x207fffff,                          // nBits
                     3,                                   // nVersion
                     treasuryDeposit,                     // genesisReward (treasury deposit)
-                   "Spor5rpxvaGQ4cW8dptj3zW94MxuAwW4P", // first treasury address
+                   "Sapor5rpxvaGQ4cW8dptj3zW94MxuAwW4P", // first treasury address
                    "SeYn7AmvjB3Wr3QqdJK6UsW85dPKeEKmSg", // second treasury address
                    "Sbe6m4y6wGhUo6FoS6Sab2PxnScXm8HQTf"  // third treasury address
                 /* - FIXME: NH - Replace the above with the values below when we have the block and correct values */
