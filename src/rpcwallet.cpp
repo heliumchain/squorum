68ea088dae95d1954a8d7f5f2250c8b49628fd8c
Fuzzbawls <fuzzbawls@gmail.com>
2018-09-09 12:46:01 -0700

    [RPC] Error when calling getreceivedbyaddress with non-wallet address
    When calling `getreceivedbyaddress` with a non-wallet address, return a
    proper error code/message instead of `0`

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -551,9 +551,9 @@ UniValue signmessage(const UniValue& params, bool fHelp)
 UniValue getreceivedbyaddress(const UniValue& params, bool fHelp)
 {
     if (fHelp || params.size() < 1 || params.size() > 2)
-        throw runtime_error(
-            "getreceivedbyaddress \"address\" ( minconf )\n"
-            "\nReturns the total amount received by the given address in transactions with at least minconf confirmations.\n"
+        throw std::runtime_error(
+            "getreceivedbyaddress \"heliumaddress\" ( minconf )\n"
+            "\nReturns the total amount received by the given heliumaddress in transactions with at least minconf confirmations.\n"
 
             "\nArguments:\n"
             "1. \"address\"  (string, required) The address for transactions.\n"
@@ -580,7 +580,7 @@ UniValue getreceivedbyaddress(const UniValue& params, bool fHelp)
         throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Invalid Helium address");
     CScript scriptPubKey = GetScriptForDestination(address.Get());
     if (!IsMine(*pwalletMain, scriptPubKey))
-        return (double)0.0;
+        throw JSONRPCError(RPC_WALLET_ERROR, "Address not found in wallet");
 
     // Minimum confirmations
     int nMinDepth = 1;
@@ -589,7 +589,7 @@ UniValue getreceivedbyaddress(const UniValue& params, bool fHelp)
 
     // Tally
     CAmount nAmount = 0;
-    for (map<uint256, CWalletTx>::iterator it = pwalletMain->mapWallet.begin(); it != pwalletMain->mapWallet.end(); ++it) {
+    for (std::map<uint256, CWalletTx>::iterator it = pwalletMain->mapWallet.begin(); it != pwalletMain->mapWallet.end(); ++it) {
         const CWalletTx& wtx = (*it).second;
         if (wtx.IsCoinBase() || !IsFinalTx(wtx))
             continue;
