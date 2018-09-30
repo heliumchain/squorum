30850e809bd03e0f38fc8dabb31381f7ca1dce32
warrows <warrows@gamer666.fr>
2018-09-30 14:47:36 +0200

    [UI] Sort numbers correctly in zPIV control dialog

--- src/qt/zhlmcontroldialog.cpp
+++ src/qt/zhlmcontroldialog.cpp
@@ -42,6 +42,15 @@ void ZHlmControlDialog::setModel(WalletModel *model)
     updateList();
 }
 
+// helper function str_pad
+QString ZHlmControlDialog::strPad(QString s, int nPadLength, QString sPadding)
+{
+    while (s.length() < nPadLength)
+        s = sPadding + s;
+
+    return s;
+}
+
 //Update the tree widget
 void ZHlmControlDialog::updateList()
 {
@@ -60,7 +69,7 @@ void ZHlmControlDialog::updateList()
         mapDenomPosition[denom] = ui->treeWidget->indexOfTopLevelItem(itemDenom);
 
         itemDenom->setFlags(flgTristate);
-        itemDenom->setText(COLUMN_DENOMINATION, QString::number(denom));
+        itemDenom->setText(COLUMN_DENOMINATION, strPad(QString::number(denom), 5, " "));
     }
 
     // select all unused coins - including not mature. Update status of coins too.
@@ -84,9 +93,9 @@ void ZHlmControlDialog::updateList()
         else
             itemMint->setCheckState(COLUMN_CHECKBOX, Qt::Unchecked);
 
-        itemMint->setText(COLUMN_DENOMINATION, QString::number(mint.denom));
+        itemMint->setText(COLUMN_DENOMINATION, strPad(QString::number(mint.denom), 5, " "));
         itemMint->setText(COLUMN_PUBCOIN, QString::fromStdString(strPubCoinHash));
-        itemMint->setText(COLUMN_VERSION, QString::number(mint.nVersion));
+        itemMint->setText(COLUMN_VERSION, strPad(QString::number(mint.nVersion), 3, " "));
 
         int nConfirmations = (mint.nHeight ? nBestHeight - mint.nHeight : 0);
         if (nConfirmations < 0) {
@@ -94,7 +103,7 @@ void ZHlmControlDialog::updateList()
             nConfirmations = 0;
         }
 
-        itemMint->setText(COLUMN_CONFIRMATIONS, QString::number(nConfirmations));
+        itemMint->setText(COLUMN_CONFIRMATIONS, strPad(QString::number(nConfirmations), 15, " "));
 
         // check for maturity
         bool isMature = false;
