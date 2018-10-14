c8de0075409873b2e26a67c9a290374b72891fd1
Fuzzbawls <fuzzbawls@gmail.com>
2018-10-14 00:24:28 -0700

    Ust std threading in sync.{h,cpp}
    Another removal of boost dependency as well as fixing up some
    `-Wthread-safety-analysis` issues.

--- src/util.h
+++ src/util.h
@@ -30,6 +30,7 @@
 
 #include <boost/filesystem/path.hpp>
 #include <boost/thread/exceptions.hpp>
+#include <boost/thread/condition_variable.hpp> // for boost::thread_interrupted
 
 //Helium only features
 
