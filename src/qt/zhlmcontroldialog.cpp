1d5ee7c3791af1c539474564376b71603f186db0
warrows <warrows@gamer666.fr>
2019-03-29 17:12:44 +0100

    [QT] Fix a display bug about zPIV mints
    When opening the zPIV coin selection in privacy tab, if the wallet is
    locked the message for all mints is that the seed is wrong. This commit
    fixes it by replacing the message and stating that the locked wallet
    prevents precomputation and spending.

--- src/qt/zhlmcontroldialog.cpp
+++ src/qt/zhlmcontroldialog.cpp
@@ -140,6 +140,8 @@ void ZHlmControlDialog::updateList()
             string strReason = "";
             if(nConfirmations < Params().Zerocoin_MintRequiredConfirmations())
                 strReason = strprintf("Needs %d more confirmations", Params().Zerocoin_MintRequiredConfirmations() - nConfirmations);
+            else if (model->getEncryptionStatus() == WalletModel::EncryptionStatus::Locked)
+                strReason = "Your wallet is locked. Impossible to precompute or spend zHLM.";
             else if (!mint.isSeedCorrect)
                 strReason = "The zHLM seed used to mint this zHLM is not the same as currently hold in the wallet";
             else
