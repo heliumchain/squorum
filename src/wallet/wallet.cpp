vampyren/PIVX

[zPIV] new protocol enforcement height added. Height not final, just randomly selected and tested on regtest.

src/wallet/wallet.cpp
@@ -4903,6 +4903,13 @@ bool CWallet::MintsToInputVectorPublicSpend(std::map<CBigNum, CZerocoinMint>& ma
 
 bool CWallet::CreateZerocoinSpendTransaction(CAmount nValue, CWalletTx& wtxNew, CReserveKey& reserveKey, CZerocoinSpendReceipt& receipt, vector<CZerocoinMint>& vSelectedMints, vector<CDeterministicMint>& vNewMints, bool fMintChange,  bool fMinimizeChange, CBitcoinAddress* address)
 {
+
+    // Check enforcement
+    if (chainActive.Tip()->nHeight < Params().Zerocoin_Block_Public_Spend_Enabled()){
+        receipt.SetStatus(_("New protocol enforcement not activated"), ZPIV_SPEND_ERROR);
+        return false;
+    }
+
     // Check available funds
     int nStatus = ZPIV_TRX_FUNDS_PROBLEMS;
     if (nValue > GetZerocoinBalance(true)) {

heliumchain/squorum

src/wallet/wallet.cpp
@@ -4905,6 +4905,13 @@ bool CWallet::MintsToInputVectorPublicSpend(std::map<CBigNum, CZerocoinMint>& ma
 
 bool CWallet::CreateZerocoinSpendTransaction(CAmount nValue, CWalletTx& wtxNew, CReserveKey& reserveKey, CZerocoinSpendReceipt& receipt, vector<CZerocoinMint>& vSelectedMints, vector<CDeterministicMint>& vNewMints, bool fMintChange,  bool fMinimizeChange, CBitcoinAddress* address)
 {
+
+    // Check enforcement
+    if (chainActive.Tip()->nHeight < Params().Zerocoin_Block_Public_Spend_Enabled()){
+        receipt.SetStatus(_("New protocol enforcement not activated"), ZHLM_SPEND_ERROR);
+        return false;
+    }
+
     // Check available funds
     int nStatus = ZHLM_TRX_FUNDS_PROBLEMS;
     if (nValue > GetZerocoinBalance(true)) {