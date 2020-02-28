furszy/PIVX-1

reorg + refactoring + conflicts

src/zpiv/accumulators.h
@@ -8,32 +8,14 @@
 #include "libzerocoin/Accumulator.h"
 #include "libzerocoin/Coin.h"
 #include "libzerocoin/Denominations.h"
-#include "primitives/zerocoin.h"
+#include "zpiv/zerocoin.h"
 #include "accumulatormap.h"
 #include "chain.h"
 #include "uint256.h"
 #include "bloom.h"
+#include "witness.h"
 
 class CBlockIndex;
-
-class CoinWitnessData
-{
-public:
-    std::unique_ptr<libzerocoin::PublicCoin> coin;
-    std::unique_ptr<libzerocoin::Accumulator> pAccumulator;
-    std::unique_ptr<libzerocoin::AccumulatorWitness> pWitness;
-    libzerocoin::CoinDenomination denom;
-    int nHeightCheckpoint;
-    int nHeightMintAdded;
-    int nHeightAccStart;
-    int nMintsAdded;
-    uint256 txid;
-    bool isV1;
-
-    CoinWitnessData();
-    void SetHeightMintAdded(int nHeight);
-    //  CoinWitnessData(CoinWitnessData&);
-};
 
 std::map<libzerocoin::CoinDenomination, int> GetMintMaturityHeight();
 

heliumchain/squorum

src/zhlm/accumulators.h
@@ -8,32 +8,14 @@
 #include "libzerocoin/Accumulator.h"
 #include "libzerocoin/Coin.h"
 #include "libzerocoin/Denominations.h"
-#include "primitives/zerocoin.h"
+#include "zhlm/zerocoin.h"
 #include "accumulatormap.h"
 #include "chain.h"
 #include "uint256.h"
 #include "bloom.h"
+#include "witness.h"
 
 class CBlockIndex;
-
-class CoinWitnessData
-{
-public:
-    std::unique_ptr<libzerocoin::PublicCoin> coin;
-    std::unique_ptr<libzerocoin::Accumulator> pAccumulator;
-    std::unique_ptr<libzerocoin::AccumulatorWitness> pWitness;
-    libzerocoin::CoinDenomination denom;
-    int nHeightCheckpoint;
-    int nHeightMintAdded;
-    int nHeightAccStart;
-    int nMintsAdded;
-    uint256 txid;
-    bool isV1;
-
-    CoinWitnessData();
-    void SetHeightMintAdded(int nHeight);
-    //  CoinWitnessData(CoinWitnessData&);
-};
 
 std::map<libzerocoin::CoinDenomination, int> GetMintMaturityHeight();
 