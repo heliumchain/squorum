furszy/PIVX-1

re-add zpivchain.h (fixes merge conflict error)

src/init.cpp
@@ -40,6 +40,7 @@
 #include "utilmoneystr.h"
 #include "validationinterface.h"
 #include "zpiv/accumulatorcheckpoints.h"
+#include "zpivchain.h"
 
 #ifdef ENABLE_WALLET
 #include "db.h"

src/test/zerocoin_implementation_tests.cpp
@@ -16,6 +16,7 @@
 #include <zpiv/accumulators.h>
 #include "wallet.h"
 #include "zpiv/zpivwallet.h"
+#include "zpivchain.h"
 
 using namespace libzerocoin;
 

heliumchain/squorum

src/init.cpp
@@ -41,6 +41,7 @@
 #include "utilmoneystr.h"
 #include "validationinterface.h"
 #include "zhlm/accumulatorcheckpoints.h"
+#include "zhlmchain.h"
 
 #ifdef ENABLE_WALLET
 #include "db.h"

src/test/zerocoin_implementation_tests.cpp
@@ -16,6 +16,7 @@
 #include <zhlm/accumulators.h>
 #include "wallet.h"
 #include "zhlm/zhlmwallet.h"
+#include "zhlmchain.h"
 
 using namespace libzerocoin;
 