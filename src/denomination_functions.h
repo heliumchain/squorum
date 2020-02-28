presstab/PIVX

More progress on caching spend data.

src/denomination_functions.h
@@ -5,7 +5,7 @@
 #include "reverse_iterate.h"
 #include "util.h"
 #include "libzerocoin/Denominations.h"
-#include "primitives/zerocoin.h"
+#include "zpiv/zerocoin.h"
 #include <list>
 #include <map>
 std::vector<CMintMeta> SelectMintsFromList(const CAmount nValueTarget, CAmount& nSelectedValue,

src/init.cpp
@@ -11,7 +11,7 @@
 
 #include "init.h"
 
-#include "accumulators.h"
+#include "zpiv/accumulators.h"
 #include "activemasternode.h"
 #include "addrman.h"
 #include "amount.h"
@@ -39,13 +39,12 @@
 #include "util.h"
 #include "utilmoneystr.h"
 #include "validationinterface.h"
-#include "accumulatorcheckpoints.h"
+#include "zpiv/accumulatorcheckpoints.h"
 
 #ifdef ENABLE_WALLET
 #include "db.h"
 #include "wallet.h"
 #include "walletdb.h"
-#include "accumulators.h"
 
 #endif
 

src/main.cpp
@@ -7,8 +7,8 @@
 
 #include "main.h"
 
-#include "accumulators.h"
-#include "accumulatormap.h"
+#include "zpiv/accumulators.h"
+#include "zpiv/accumulatormap.h"
 #include "addrman.h"
 #include "alert.h"
 #include "blocksignature.h"
@@ -34,7 +34,7 @@
 #include "utilmoneystr.h"
 #include "validationinterface.h"
 
-#include "primitives/zerocoin.h"
+#include "zpiv/zerocoin.h"
 #include "libzerocoin/Denominations.h"
 #include "invalid.h"
 

src/qt/privacydialog.cpp
@@ -22,8 +22,8 @@
 #include <QSettings>
 #include <utilmoneystr.h>
 #include <QtWidgets>
-#include <primitives/deterministicmint.h>
-#include <accumulators.h>
+#include <zpiv/deterministicmint.h>
+#include <zpiv/accumulators.h>
 
 PrivacyDialog::PrivacyDialog(QWidget* parent) : QDialog(parent),
                                                           ui(new Ui::PrivacyDialog),

src/test/zerocoin_implementation_tests.cpp
@@ -7,15 +7,15 @@
 #include "chainparams.h"
 #include "main.h"
 #include "txdb.h"
-#include "primitives/deterministicmint.h"
+#include "zpiv/deterministicmint.h"
 #include "key.h"
-#include "accumulatorcheckpoints.h"
+#include "zpiv/accumulatorcheckpoints.h"
 #include "libzerocoin/bignum.h"
 #include <boost/test/unit_test.hpp>
 #include <iostream>
-#include <accumulators.h>
+#include <zpiv/accumulators.h>
 #include "wallet.h"
-#include "zpivwallet.h"
+#include "zpiv/zpivwallet.h"
 
 using namespace libzerocoin;
 

src/walletdb.h
@@ -11,10 +11,10 @@
 #include "db.h"
 #include "key.h"
 #include "keystore.h"
-#include "primitives/zerocoin.h"
+#include "zpiv/zerocoin.h"
 #include "libzerocoin/Accumulator.h"
 #include "libzerocoin/Denominations.h"
-#include "zpivtracker.h"
+#include "zpiv/zpivtracker.h"
 
 #include <list>
 #include <stdint.h>

src/zpiv/mintpool.h
@@ -8,7 +8,7 @@
 #include <map>
 #include <list>
 
-#include "primitives/zerocoin.h"
+#include "zpiv/zerocoin.h"
 #include "libzerocoin/bignum.h"
 #include "uint256.h"
 

src/zpiv/witness.h
@@ -1,0 +1,30 @@
+#ifndef PIVX_WITNESS_H
+#define PIVX_WITNESS_H
+
+
+#include <libzerocoin/Accumulator.h>
+#include <libzerocoin/Coin.h>
+#include "zerocoin.h"
+
+class CoinWitnessData
+{
+public:
+    std::unique_ptr<libzerocoin::PublicCoin> coin;
+    std::unique_ptr<libzerocoin::Accumulator> pAccumulator;
+    std::unique_ptr<libzerocoin::AccumulatorWitness> pWitness;
+    libzerocoin::CoinDenomination denom;
+    int nHeightCheckpoint;
+    int nHeightMintAdded;
+    int nHeightAccStart;
+    int nHeightAccEnd;
+    int nMintsAdded;
+    uint256 txid;
+    bool isV1;
+
+    CoinWitnessData();
+    CoinWitnessData(CZerocoinMint& mint);
+    void SetHeightMintAdded(int nHeight);
+    void SetNull();
+};
+
+#endif //PIVX_WITNESS_H

heliumchain/squorum

src/denomination_functions.h
@@ -5,7 +5,7 @@
 #include "reverse_iterate.h"
 #include "util.h"
 #include "libzerocoin/Denominations.h"
-#include "primitives/zerocoin.h"
+#include "zhlm/zerocoin.h"
 #include <list>
 #include <map>
 std::vector<CMintMeta> SelectMintsFromList(const CAmount nValueTarget, CAmount& nSelectedValue,

src/init.cpp
@@ -12,8 +12,7 @@
 
 #include "init.h"
 
-#include "accumulatorcheckpoints.h"
-#include "accumulators.h"
+#include "zhlm/accumulators.h"
 #include "activemasternode.h"
 #include "addrman.h"
 #include "amount.h"
@@ -41,13 +40,12 @@
 #include "util.h"
 #include "utilmoneystr.h"
 #include "validationinterface.h"
-#include "zhlmchain.h"
+#include "zhlm/accumulatorcheckpoints.h"
 
 #ifdef ENABLE_WALLET
 #include "db.h"
 #include "wallet.h"
 #include "walletdb.h"
-#include "accumulators.h"
 
 #endif
 

src/main.cpp
@@ -8,8 +8,8 @@
 
 #include "main.h"
 
-#include "accumulators.h"
-#include "accumulatormap.h"
+#include "zhlm/accumulators.h"
+#include "zhlm/accumulatormap.h"
 #include "addrman.h"
 #include "alert.h"
 #include "blocksignature.h"
@@ -36,7 +36,7 @@
 #include "validationinterface.h"
 #include "zhlmchain.h"
 
-#include "primitives/zerocoin.h"
+#include "zhlm/zerocoin.h"
 #include "libzerocoin/Denominations.h"
 #include "invalid.h"
 #include <sstream>

src/qt/privacydialog.cpp
@@ -22,8 +22,8 @@
 #include <QSettings>
 #include <utilmoneystr.h>
 #include <QtWidgets>
-#include <primitives/deterministicmint.h>
-#include <accumulators.h>
+#include <zhlm/deterministicmint.h>
+#include <zhlm/accumulators.h>
 
 PrivacyDialog::PrivacyDialog(QWidget* parent) : QDialog(parent, Qt::WindowSystemMenuHint | Qt::WindowTitleHint | Qt::WindowCloseButtonHint),
                                                           ui(new Ui::PrivacyDialog),

src/test/zerocoin_implementation_tests.cpp
@@ -7,16 +7,15 @@
 #include "chainparams.h"
 #include "main.h"
 #include "txdb.h"
-#include "primitives/deterministicmint.h"
+#include "zhlm/deterministicmint.h"
 #include "key.h"
-#include "accumulatorcheckpoints.h"
+#include "zhlm/accumulatorcheckpoints.h"
 #include "libzerocoin/bignum.h"
 #include <boost/test/unit_test.hpp>
 #include <iostream>
-#include <accumulators.h>
+#include <zhlm/accumulators.h>
 #include "wallet.h"
-#include "zhlmwallet.h"
-#include "zhlmchain.h"
+#include "zhlm/zhlmwallet.h"
 
 using namespace libzerocoin;
 

src/walletdb.h
@@ -11,10 +11,10 @@
 #include "db.h"
 #include "key.h"
 #include "keystore.h"
-#include "primitives/zerocoin.h"
+#include "zhlm/zerocoin.h"
 #include "libzerocoin/Accumulator.h"
 #include "libzerocoin/Denominations.h"
-#include "zhlmtracker.h"
+#include "zhlm/zhlmtracker.h"
 
 #include <list>
 #include <stdint.h>

src/zhlm/mintpool.h
@@ -8,7 +8,7 @@
 #include <map>
 #include <list>
 
-#include "primitives/zerocoin.h"
+#include "zhlm/zerocoin.h"
 #include "libzerocoin/bignum.h"
 #include "uint256.h"
 

src/zhlm/witness.h
@@ -1,0 +1,30 @@
+#ifndef Helium_WITNESS_H
+#define Helium_WITNESS_H
+
+
+#include <libzerocoin/Accumulator.h>
+#include <libzerocoin/Coin.h>
+#include "zerocoin.h"
+
+class CoinWitnessData
+{
+public:
+    std::unique_ptr<libzerocoin::PublicCoin> coin;
+    std::unique_ptr<libzerocoin::Accumulator> pAccumulator;
+    std::unique_ptr<libzerocoin::AccumulatorWitness> pWitness;
+    libzerocoin::CoinDenomination denom;
+    int nHeightCheckpoint;
+    int nHeightMintAdded;
+    int nHeightAccStart;
+    int nHeightAccEnd;
+    int nMintsAdded;
+    uint256 txid;
+    bool isV1;
+
+    CoinWitnessData();
+    CoinWitnessData(CZerocoinMint& mint);
+    void SetHeightMintAdded(int nHeight);
+    void SetNull();
+};
+
+#endif //Helium_WITNESS_H