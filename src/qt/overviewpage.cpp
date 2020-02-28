eons-finance/ALQO

Simplify watch only balance display logic
Instead of making the watch-only balances independent, make them inherit
 their visible state from non watch-only balances. This prevents the
 situation where a watch-only line item is blank.

src/qt/overviewpage.cpp
@@ -281,28 +281,28 @@ void OverviewPage::setBalance(const CAmount& balance, const CAmount& unconfirmed
 
     // PIV Available
     bool showPIVAvailable = settingShowAllBalances || pivAvailableBalance != nTotalBalance;
-    bool showWatchOnlyPIVAvailable = settingShowAllBalances || nAvailableWatchBalance != nTotalWatchBalance;
+    bool showWatchOnlyPIVAvailable = showPIVAvailable || nAvailableWatchBalance != nTotalWatchBalance;
     ui->labelBalanceText->setVisible(showPIVAvailable || showWatchOnlyPIVAvailable);
     ui->labelBalance->setVisible(showPIVAvailable || showWatchOnlyPIVAvailable);
     ui->labelWatchAvailable->setVisible(showWatchOnlyPIVAvailable && showWatchOnly);
 
     // PIV Pending
     bool showPIVPending = settingShowAllBalances || unconfirmedBalance != 0;
-    bool showWatchOnlyPIVPending = settingShowAllBalances || watchUnconfBalance != 0;
+    bool showWatchOnlyPIVPending = showPIVPending || watchUnconfBalance != 0;
     ui->labelPendingText->setVisible(showPIVPending || showWatchOnlyPIVPending);
     ui->labelUnconfirmed->setVisible(showPIVPending || showWatchOnlyPIVPending);
     ui->labelWatchPending->setVisible(showWatchOnlyPIVPending && showWatchOnly);
 
     // PIV Immature
     bool showPIVImmature = settingShowAllBalances || immatureBalance != 0;
-    bool showWatchOnlyImmature = settingShowAllBalances || watchImmatureBalance != 0;
+    bool showWatchOnlyImmature = showPIVImmature || watchImmatureBalance != 0;
     ui->labelImmatureText->setVisible(showPIVImmature || showWatchOnlyImmature);
     ui->labelImmature->setVisible(showPIVImmature || showWatchOnlyImmature); // for symmetry reasons also show immature label when the watch-only one is shown
     ui->labelWatchImmature->setVisible(showWatchOnlyImmature && showWatchOnly); // show watch-only immature balance
 
     // PIV Locked
     bool showPIVLocked = settingShowAllBalances || nLockedBalance != 0;
-    bool showWatchOnlyPIVLocked = settingShowAllBalances || nWatchOnlyLockedBalance != 0;
+    bool showWatchOnlyPIVLocked = showPIVLocked || nWatchOnlyLockedBalance != 0;
     ui->labelLockedBalanceText->setVisible(showPIVLocked || showWatchOnlyPIVLocked);
     ui->labelLockedBalance->setVisible(showPIVLocked || showWatchOnlyPIVLocked);
     ui->labelWatchLocked->setVisible(showWatchOnlyPIVLocked && showWatchOnly);

heliumchain/squorum

src/qt/overviewpage.cpp
@@ -275,28 +275,28 @@ void OverviewPage::setBalance(const CAmount& balance, const CAmount& unconfirmed
 
     // HLM Available
     bool showHLMAvailable = settingShowAllBalances || hlmAvailableBalance != nTotalBalance;
-    bool showWatchOnlyHLMAvailable = settingShowAllBalances || nAvailableWatchBalance != nTotalWatchBalance;
+    bool showWatchOnlyHLMAvailable = showHLMAvailable || nAvailableWatchBalance != nTotalWatchBalance;
     ui->labelBalanceText->setVisible(showHLMAvailable || showWatchOnlyHLMAvailable);
     ui->labelBalance->setVisible(showHLMAvailable || showWatchOnlyHLMAvailable);
     ui->labelWatchAvailable->setVisible(showWatchOnlyHLMAvailable && showWatchOnly);
 
     // HLM Pending
     bool showHLMPending = settingShowAllBalances || unconfirmedBalance != 0;
-    bool showWatchOnlyHLMPending = settingShowAllBalances || watchUnconfBalance != 0;
+    bool showWatchOnlyHLMPending = showHLMPending || watchUnconfBalance != 0;
     ui->labelPendingText->setVisible(showHLMPending || showWatchOnlyHLMPending);
     ui->labelUnconfirmed->setVisible(showHLMPending || showWatchOnlyHLMPending);
     ui->labelWatchPending->setVisible(showWatchOnlyHLMPending && showWatchOnly);
 
     // HLM Immature
     bool showHLMImmature = settingShowAllBalances || immatureBalance != 0;
-    bool showWatchOnlyImmature = settingShowAllBalances || watchImmatureBalance != 0;
+    bool showWatchOnlyImmature = showHLMImmature || watchImmatureBalance != 0;
     ui->labelImmatureText->setVisible(showHLMImmature || showWatchOnlyImmature);
     ui->labelImmature->setVisible(showHLMImmature || showWatchOnlyImmature); // for symmetry reasons also show immature label when the watch-only one is shown
     ui->labelWatchImmature->setVisible(showWatchOnlyImmature && showWatchOnly); // show watch-only immature balance
 
     // HLM Locked
     bool showHLMLocked = settingShowAllBalances || nLockedBalance != 0;
-    bool showWatchOnlyHLMLocked = settingShowAllBalances || nWatchOnlyLockedBalance != 0;
+    bool showWatchOnlyHLMLocked = showHLMLocked || nWatchOnlyLockedBalance != 0;
     ui->labelLockedBalanceText->setVisible(showHLMLocked || showWatchOnlyHLMLocked);
     ui->labelLockedBalance->setVisible(showHLMLocked || showWatchOnlyHLMLocked);
     ui->labelWatchLocked->setVisible(showWatchOnlyHLMLocked && showWatchOnly);