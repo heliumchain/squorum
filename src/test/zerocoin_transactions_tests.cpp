furszy/PIVX-1

[UNIT TEST] publicCoinSpend valid input creation and verification.
[UNIT TEST] publicCoinSpend creation + validation completed.

src/test/zerocoin_transactions_tests.cpp
@@ -3,6 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "libzerocoin/Denominations.h"
+#include "libzerocoin/Coin.h"
 #include "amount.h"
 #include "chainparams.h"
 #include "coincontrol.h"
@@ -10,6 +11,7 @@
 #include "wallet/wallet.h"
 #include "wallet/walletdb.h"
 #include "txdb.h"
+#include "zpiv/zpivmodule.h"
 #include <boost/test/unit_test.hpp>
 #include <iostream>
 
@@ -50,6 +52,64 @@ BOOST_AUTO_TEST_CASE(zerocoin_spend_test)
     /// BOOST_CHECK_MESSAGE(vString == "Error: Wallet locked, unable to create transaction!"," Locked Wallet Check Failed");
 
     BOOST_CHECK_MESSAGE(receipt2.GetStatus() == ZPIV_TRX_FUNDS_PROBLEMS, "Failed Invalid Amount Check");
+
+}
+
+BOOST_AUTO_TEST_CASE(zerocoin_public_spend_test)
+{
+    SelectParams(CBaseChainParams::MAIN);
+    ZerocoinParams *ZCParams = Params().Zerocoin_Params(false);
+    (void)ZCParams;
+
+    ZPIVModule zpivModule;
+
+    PrivateCoin privCoin(ZCParams, libzerocoin::CoinDenomination::ZQ_ONE, true);
+    const CPrivKey privKey = privCoin.getPrivKey();
+
+    CZerocoinMint mint = CZerocoinMint(
+            privCoin.getPublicCoin().getDenomination(),
+            privCoin.getPublicCoin().getValue(),
+            privCoin.getRandomness(),
+            privCoin.getSerialNumber(),
+            false,
+            privCoin.getVersion(),
+            nullptr);
+    mint.SetPrivKey(privKey);
+
+
+    // Mint tx
+    CTransaction prevTx;
+
+    CScript scriptSerializedCoin = CScript()
+    << OP_ZEROCOINMINT << privCoin.getPublicCoin().getValue().getvch().size() << privCoin.getPublicCoin().getValue().getvch();
+    CTxOut out = CTxOut(libzerocoin::ZerocoinDenominationToAmount(privCoin.getPublicCoin().getDenomination()), scriptSerializedCoin);
+    prevTx.vout.push_back(out);
+
+    mint.SetOutputIndex(0);
+    mint.SetTxHash(prevTx.GetHash());
+
+    // Spend tx
+    CMutableTransaction tx;
+    tx.vout.resize(1);
+    tx.vout[0].nValue = 1*CENT;
+    tx.vout[0].scriptPubKey = GetScriptForDestination(CBitcoinAddress("D9Ti4LEhF1n6dR2hGd2SyNADD51AVgva6q").Get());
+
+    CTxIn in;
+    if (!zpivModule.createInput(in, mint, tx.GetHash())){
+        BOOST_CHECK_MESSAGE(false, "Failed to create zc input");
+    }
+
+    PublicCoinSpend publicSpend(ZCParams);
+    if (!zpivModule.validateInput(in, out, tx, publicSpend)){
+        BOOST_CHECK_MESSAGE(false, "Failed to validate zc input");
+    }
+
+    PublicCoinSpend publicSpendTest(ZCParams);
+    BOOST_CHECK_MESSAGE(zpivModule.parseCoinSpend(in, tx, out, publicSpendTest), "Failed to parse public spend");
+    libzerocoin::CoinSpend *spend = &publicSpendTest;
+
+    BOOST_CHECK_MESSAGE(publicSpendTest.HasValidSignature(), "Failed to validate public spend signature");
+    BOOST_CHECK_MESSAGE(spend->HasValidSignature(), "Failed to validate spend signature");
 
 }
 

heliumchain/squorum

src/test/zerocoin_transactions_tests.cpp
@@ -3,6 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "libzerocoin/Denominations.h"
+#include "libzerocoin/Coin.h"
 #include "amount.h"
 #include "chainparams.h"
 #include "coincontrol.h"
@@ -10,6 +11,7 @@
 #include "wallet/wallet.h"
 #include "wallet/walletdb.h"
 #include "txdb.h"
+#include "zhlm/zhlmmodule.h"
 #include <boost/test/unit_test.hpp>
 #include <iostream>
 
@@ -50,6 +52,64 @@ BOOST_AUTO_TEST_CASE(zerocoin_spend_test)
     /// BOOST_CHECK_MESSAGE(vString == "Error: Wallet locked, unable to create transaction!"," Locked Wallet Check Failed");
 
     BOOST_CHECK_MESSAGE(receipt2.GetStatus() == ZHLM_TRX_FUNDS_PROBLEMS, "Failed Invalid Amount Check");
+
+}
+
+BOOST_AUTO_TEST_CASE(zerocoin_public_spend_test)
+{
+    SelectParams(CBaseChainParams::MAIN);
+    ZerocoinParams *ZCParams = Params().Zerocoin_Params(false);
+    (void)ZCParams;
+
+    ZHLMModule zhlmModule;
+
+    PrivateCoin privCoin(ZCParams, libzerocoin::CoinDenomination::ZQ_ONE, true);
+    const CPrivKey privKey = privCoin.getPrivKey();
+
+    CZerocoinMint mint = CZerocoinMint(
+            privCoin.getPublicCoin().getDenomination(),
+            privCoin.getPublicCoin().getValue(),
+            privCoin.getRandomness(),
+            privCoin.getSerialNumber(),
+            false,
+            privCoin.getVersion(),
+            nullptr);
+    mint.SetPrivKey(privKey);
+
+
+    // Mint tx
+    CTransaction prevTx;
+
+    CScript scriptSerializedCoin = CScript()
+    << OP_ZEROCOINMINT << privCoin.getPublicCoin().getValue().getvch().size() << privCoin.getPublicCoin().getValue().getvch();
+    CTxOut out = CTxOut(libzerocoin::ZerocoinDenominationToAmount(privCoin.getPublicCoin().getDenomination()), scriptSerializedCoin);
+    prevTx.vout.push_back(out);
+
+    mint.SetOutputIndex(0);
+    mint.SetTxHash(prevTx.GetHash());
+
+    // Spend tx
+    CMutableTransaction tx;
+    tx.vout.resize(1);
+    tx.vout[0].nValue = 1*CENT;
+    tx.vout[0].scriptPubKey = GetScriptForDestination(CBitcoinAddress("D9Ti4LEhF1n6dR2hGd2SyNADD51AVgva6q").Get());
+
+    CTxIn in;
+    if (!zhlmModule.createInput(in, mint, tx.GetHash())){
+        BOOST_CHECK_MESSAGE(false, "Failed to create zc input");
+    }
+
+    PublicCoinSpend publicSpend(ZCParams);
+    if (!zhlmModule.validateInput(in, out, tx, publicSpend)){
+        BOOST_CHECK_MESSAGE(false, "Failed to validate zc input");
+    }
+
+    PublicCoinSpend publicSpendTest(ZCParams);
+    BOOST_CHECK_MESSAGE(zhlmModule.parseCoinSpend(in, tx, out, publicSpendTest), "Failed to parse public spend");
+    libzerocoin::CoinSpend *spend = &publicSpendTest;
+
+    BOOST_CHECK_MESSAGE(publicSpendTest.HasValidSignature(), "Failed to validate public spend signature");
+    BOOST_CHECK_MESSAGE(spend->HasValidSignature(), "Failed to validate spend signature");
 
 }
 