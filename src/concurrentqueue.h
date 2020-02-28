furszy/bitcoin-core

Zerocoin Light Node Protocol

src/concurrentqueue.h
@@ -1,0 +1,50 @@
+//
+// Copyright (c) 2015-2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef PIVX_CONCURRENTQUEUE_H
+#define PIVX_CONCURRENTQUEUE_H
+
+
+#include <mutex>
+#include <condition_variable>
+#include <deque>
+
+template <typename T>
+class concurrentqueue
+{
+private:
+    std::mutex              mutex;
+    std::condition_variable condition;
+    std::deque<T>           queue;
+public:
+    void push(T const& value) {
+        {
+            std::unique_lock<std::mutex> lock(this->mutex);
+            queue.push_front(value);
+        }
+        this->condition.notify_one();
+    }
+    T pop() {
+        std::unique_lock<std::mutex> lock(this->mutex);
+        this->condition.wait(lock, [=]{ return !this->queue.empty(); });
+        T rc(std::move(this->queue.back()));
+        this->queue.pop_back();
+        return rc;
+    }
+
+    T popNotWait(){
+        std::unique_lock<std::mutex> lock(this->mutex);
+        T rc(std::move(this->queue.back()));
+        this->queue.pop_back();
+        return rc;
+    }
+
+    bool hasElements(){
+        std::unique_lock<std::mutex> lock(this->mutex);
+        return !queue.empty();
+    }
+};
+
+#endif //PIVX_CONCURRENTQUEUE_H

src/genwit.h
@@ -1,0 +1,66 @@
+// Copyright (c) 2015-2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef PIVX_GENWIT_H
+#define PIVX_GENWIT_H
+
+
+#include <iostream>
+#include "bloom.h"
+#include "libzerocoin/Denominations.h"
+#include "net.h"
+
+class CGenWit {
+
+    public:
+
+    CGenWit();
+
+    CGenWit(const CBloomFilter &filter, int startingHeight, libzerocoin::CoinDenomination den, int requestNum, CBigNum accWitValue = 0);
+
+    bool isValid(int chainActiveHeight);
+
+    ADD_SERIALIZE_METHODS;
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
+        READWRITE(filter);
+        filter.setFull();
+        READWRITE(startingHeight);
+        READWRITE(den);
+        READWRITE(requestNum);
+        try {
+            // TODO: This is for the old testnet nodes that are running my code..
+            READWRITE(accWitValue);
+        }catch (std::exception& e){
+            std::cout << e.what() << std::endl;
+        }
+    }
+
+    const CBloomFilter &getFilter() const;
+
+    int getStartingHeight() const;
+
+    libzerocoin::CoinDenomination getDen() const;
+
+    int getRequestNum() const;
+
+    CNode *getPfrom() const;
+
+    void setPfrom(CNode *pfrom);
+
+    const CBigNum &getAccWitValue() const;
+
+    const std::string toString() const;
+
+private:
+    CBloomFilter filter;
+    int startingHeight;
+    libzerocoin::CoinDenomination den;
+    int requestNum;
+    CBigNum accWitValue;
+    CNode* pfrom;
+};
+
+
+#endif //PIVX_GENWIT_H

heliumchain/squorum

src/concurrentqueue.h
@@ -1,0 +1,50 @@
+//
+// Copyright (c) 2015-2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef Helium_CONCURRENTQUEUE_H
+#define Helium_CONCURRENTQUEUE_H
+
+
+#include <mutex>
+#include <condition_variable>
+#include <deque>
+
+template <typename T>
+class concurrentqueue
+{
+private:
+    std::mutex              mutex;
+    std::condition_variable condition;
+    std::deque<T>           queue;
+public:
+    void push(T const& value) {
+        {
+            std::unique_lock<std::mutex> lock(this->mutex);
+            queue.push_front(value);
+        }
+        this->condition.notify_one();
+    }
+    T pop() {
+        std::unique_lock<std::mutex> lock(this->mutex);
+        this->condition.wait(lock, [=]{ return !this->queue.empty(); });
+        T rc(std::move(this->queue.back()));
+        this->queue.pop_back();
+        return rc;
+    }
+
+    T popNotWait(){
+        std::unique_lock<std::mutex> lock(this->mutex);
+        T rc(std::move(this->queue.back()));
+        this->queue.pop_back();
+        return rc;
+    }
+
+    bool hasElements(){
+        std::unique_lock<std::mutex> lock(this->mutex);
+        return !queue.empty();
+    }
+};
+
+#endif //Helium_CONCURRENTQUEUE_H

src/genwit.h
@@ -1,0 +1,66 @@
+// Copyright (c) 2015-2018 The PIVX developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef Helium_GENWIT_H
+#define Helium_GENWIT_H
+
+
+#include <iostream>
+#include "bloom.h"
+#include "libzerocoin/Denominations.h"
+#include "net.h"
+
+class CGenWit {
+
+    public:
+
+    CGenWit();
+
+    CGenWit(const CBloomFilter &filter, int startingHeight, libzerocoin::CoinDenomination den, int requestNum, CBigNum accWitValue = 0);
+
+    bool isValid(int chainActiveHeight);
+
+    ADD_SERIALIZE_METHODS;
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
+        READWRITE(filter);
+        filter.setFull();
+        READWRITE(startingHeight);
+        READWRITE(den);
+        READWRITE(requestNum);
+        try {
+            // TODO: This is for the old testnet nodes that are running my code..
+            READWRITE(accWitValue);
+        }catch (std::exception& e){
+            std::cout << e.what() << std::endl;
+        }
+    }
+
+    const CBloomFilter &getFilter() const;
+
+    int getStartingHeight() const;
+
+    libzerocoin::CoinDenomination getDen() const;
+
+    int getRequestNum() const;
+
+    CNode *getPfrom() const;
+
+    void setPfrom(CNode *pfrom);
+
+    const CBigNum &getAccWitValue() const;
+
+    const std::string toString() const;
+
+private:
+    CBloomFilter filter;
+    int startingHeight;
+    libzerocoin::CoinDenomination den;
+    int requestNum;
+    CBigNum accWitValue;
+    CNode* pfrom;
+};
+
+
+#endif //Helium_GENWIT_H