eons-finance/ALQO

[Tests] Add Spork functional test and update RegTest spork key

test/functional/rpc_spork.py
@@ -1,0 +1,43 @@
+#!/usr/bin/env python3
+# Copyright (c) 2019 The PIVX developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# -*- coding: utf-8 -*-
+
+from test_framework.test_framework import BitcoinTestFramework
+
+class PIVX_RPCSporkTest(BitcoinTestFramework):
+    def set_test_params(self):
+        self.setup_clean_chain = True
+        self.num_nodes = 1
+        self.extra_args = [['-staking=1', '-sporkkey=932HEevBSujW2ud7RfB1YF91AFygbBRQj3de3LyaCRqNzKKgWXi']]
+
+    def printDict(self, d):
+        self.log.info("{")
+        for k in d:
+            self.log.info("  %s = %d" % (k, d[k]))
+        self.log.info("}")
+
+
+    def run_test(self):
+        self.description = "Performs tests on the Spork RPC"
+        # check spork values:
+        sporks = self.nodes[0].spork("show")
+        self.printDict(sporks)
+        active = self.nodes[0].spork("active")
+        assert(not active["SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT"])
+        # activate SPORK 8
+        new_value = 1563253447
+        res = self.nodes[0].spork("SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT", new_value)
+        assert(res == "success")
+        sporks = self.nodes[0].spork("show")
+        self.printDict(sporks)
+        assert(sporks["SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT"] == new_value)
+        active = self.nodes[0].spork("active")
+        assert (active["SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT"])
+
+
+
+if __name__ == '__main__':
+    PIVX_RPCSporkTest().main()
+

heliumchain/squorum

test/functional/rpc_spork.py
@@ -1,0 +1,43 @@
+#!/usr/bin/env python3
+# Copyright (c) 2019 The PIVX developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+# -*- coding: utf-8 -*-
+
+from test_framework.test_framework import BitcoinTestFramework
+
+class Helium_RPCSporkTest(BitcoinTestFramework):
+    def set_test_params(self):
+        self.setup_clean_chain = True
+        self.num_nodes = 1
+        self.extra_args = [['-staking=1', '-sporkkey=932HEevBSujW2ud7RfB1YF91AFygbBRQj3de3LyaCRqNzKKgWXi']]
+
+    def printDict(self, d):
+        self.log.info("{")
+        for k in d:
+            self.log.info("  %s = %d" % (k, d[k]))
+        self.log.info("}")
+
+
+    def run_test(self):
+        self.description = "Performs tests on the Spork RPC"
+        # check spork values:
+        sporks = self.nodes[0].spork("show")
+        self.printDict(sporks)
+        active = self.nodes[0].spork("active")
+        assert(not active["SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT"])
+        # activate SPORK 8
+        new_value = 1563253447
+        res = self.nodes[0].spork("SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT", new_value)
+        assert(res == "success")
+        sporks = self.nodes[0].spork("show")
+        self.printDict(sporks)
+        assert(sporks["SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT"] == new_value)
+        active = self.nodes[0].spork("active")
+        assert (active["SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT"])
+
+
+
+if __name__ == '__main__':
+    Helium_RPCSporkTest().main()
+