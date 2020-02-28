eons-finance/ALQO

[Review] Move block identification to chainparams to support testnet also

src/main.cpp
@@ -4511,9 +4511,7 @@ bool CheckWork(const CBlock block, CBlockIndex* const pindexPrev)
 
     if (block.nBits != nBitsRequired) {
         // Pivx Specific reference to the block with the wrong threshold was used.
-        static const int64_t PivxBadBlockTime = 1471401614;
-        static const unsigned int PivxBadBlocknBits = 0x1c056dac;
-        if ((block.nTime == PivxBadBlockTime) && (block.nBits == PivxBadBlocknBits)) {
+        if ((block.nTime == Params().PivxBadBlockTime()) && (block.nBits == Params().PivxBadBlocknBits())) {
             // accept PIVX block minted with incorrect proof of work threshold
             return true;
         }

heliumchain/squorum

src/main.cpp
@@ -4231,9 +4231,7 @@ bool CheckWork(const CBlock block, CBlockIndex* const pindexPrev)
 
     if (block.nBits != nBitsRequired) {
         // Helium Specific reference to the block with the wrong threshold was used.
-        static const int64_t HeliumBadBlockTime = 1471401614;
-        static const unsigned int HeliumBadBlocknBits = 0x1c056dac;
-        if ((block.nTime == HeliumBadBlockTime) && (block.nBits == HeliumBadBlocknBits)) {
+        if ((block.nTime == Params().HeliumBadBlockTime()) && (block.nBits == Params().HeliumBadBlocknBits())) {
             // accept Helium block minted with incorrect proof of work threshold
             return true;
         }