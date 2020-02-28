eons-finance/ALQO

Remove security level in `DoZpivSpend()` call

src/rpcwallet.cpp
@@ -3758,7 +3758,7 @@ UniValue spendrawzerocoin(const UniValue& params, bool fHelp)
     vector<CZerocoinMint> vMintsSelected = {mint};
     CAmount nAmount = mint.GetDenominationAsAmount();
 
-    return DoZpivSpend(nAmount, false, true, 42, vMintsSelected, address_str);
+    return DoZpivSpend(nAmount, false, true, vMintsSelected, address_str);
 }
 
 UniValue clearspendcache(const UniValue& params, bool fHelp)

heliumchain/squorum

src/rpcwallet.cpp
@@ -3754,7 +3754,7 @@ UniValue spendrawzerocoin(const UniValue& params, bool fHelp)
     vector<CZerocoinMint> vMintsSelected = {mint};
     CAmount nAmount = mint.GetDenominationAsAmount();
 
-    return DoZhlmSpend(nAmount, false, true, 42, vMintsSelected, address_str);
+    return DoZhlmSpend(nAmount, false, true, vMintsSelected, address_str);
 }
 
 UniValue clearspendcache(const UniValue& params, bool fHelp)