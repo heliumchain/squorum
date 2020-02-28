furszy/PIVX-1

Add clearspendcache RPC command

src/rpc/server.cpp
@@ -458,7 +458,8 @@ static const CRPCCommand vRPCCommands[] =
         {"zerocoin", "setzpivseed", &setzpivseed, false, false, true},
         {"zerocoin", "generatemintlist", &generatemintlist, false, false, true},
         {"zerocoin", "searchdzpiv", &searchdzpiv, false, false, true},
-        {"zerocoin", "dzpivstate", &dzpivstate, false, false, true}
+        {"zerocoin", "dzpivstate", &dzpivstate, false, false, true},
+        {"zerocoin", "clearspendcache", &clearspendcache, false, false, true}
 
 #endif // ENABLE_WALLET
 };

src/rpcwallet.cpp
@@ -3670,3 +3670,27 @@ UniValue createautomintaddress(const UniValue& params, bool fHelp)
     CBitcoinAddress address = pwalletMain->GenerateNewAutoMintKey();
     return address.ToString();
 }
+UniValue clearspendcache(const UniValue& params, bool fHelp)
+{
+    if(fHelp || params.size() != 0)
+        throw runtime_error(
+            "clearspendcache\n"
+            "\nClear the pre-computed zPIV spend cache.\n" +
+            HelpRequiringPassphrase() + "\n"
+
+            "\nExamples\n" +
+            HelpExampleCli("clearspendcache", "") + HelpExampleRpc("clearspendcache", ""));
+
+    EnsureWalletIsUnlocked();
+
+    CzPIVTracker* zpivTracker = pwalletMain->zpivTracker.get();
+
+    {
+        TRY_LOCK(zpivTracker->cs_spendcache, fLocked);
+        if (fLocked) {
+            if (zpivTracker->ClearSpendCache())
+                return NullUniValue;
+        }
+    }
+    throw JSONRPCError(RPC_WALLET_ERROR, "Error: Spend cache not cleared!");
+}

heliumchain/squorum

src/rpc/server.cpp
@@ -472,7 +472,8 @@ static const CRPCCommand vRPCCommands[] =
         {"zerocoin", "setzhlmseed", &setzhlmseed, false, false, true},
         {"zerocoin", "generatemintlist", &generatemintlist, false, false, true},
         {"zerocoin", "searchdzhlm", &searchdzhlm, false, false, true},
-        {"zerocoin", "dzhlmstate", &dzhlmstate, false, false, true}
+        {"zerocoin", "dzhlmstate", &dzhlmstate, false, false, true},
+        {"zerocoin", "clearspendcache", &clearspendcache, false, false, true}
 
 #endif // ENABLE_WALLET
 };

src/rpcwallet.cpp
@@ -3748,3 +3748,28 @@ UniValue spendrawzerocoin(const UniValue& params, bool fHelp)
     return DoZhlmSpend(nAmount, false, true, 42, vMintsSelected, address_str);
 }
 
+
+UniValue clearspendcache(const UniValue& params, bool fHelp)
+{
+    if(fHelp || params.size() != 0)
+        throw runtime_error(
+            "clearspendcache\n"
+            "\nClear the pre-computed zHLM spend cache.\n" +
+            HelpRequiringPassphrase() + "\n"
+
+            "\nExamples\n" +
+            HelpExampleCli("clearspendcache", "") + HelpExampleRpc("clearspendcache", ""));
+
+    EnsureWalletIsUnlocked();
+
+    CzHLMTracker* zhlmTracker = pwalletMain->zhlmTracker.get();
+
+    {
+        TRY_LOCK(zhlmTracker->cs_spendcache, fLocked);
+        if (fLocked) {
+            if (zhlmTracker->ClearSpendCache())
+                return NullUniValue;
+        }
+    }
+    throw JSONRPCError(RPC_WALLET_ERROR, "Error: Spend cache not cleared!");
+}