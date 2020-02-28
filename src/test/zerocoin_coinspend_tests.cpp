eons-finance/ALQO

Fixup coinspend unit test header includes

src/test/zerocoin_coinspend_tests.cpp
@@ -5,9 +5,9 @@
 #include "libzerocoin/Denominations.h"
 #include "libzerocoin/CoinSpend.h"
 #include "libzerocoin/Accumulator.h"
-#include "primitives/zerocoin.h"
-#include "primitives/deterministicmint.h"
-#include "zpivwallet.h"
+#include "zpiv/zerocoin.h"
+#include "zpiv/deterministicmint.h"
+#include "zpiv/zpivwallet.h"
 #include "libzerocoin/Coin.h"
 #include "amount.h"
 #include "chainparams.h"

heliumchain/squorum

src/test/zerocoin_coinspend_tests.cpp
@@ -5,9 +5,9 @@
 #include "libzerocoin/Denominations.h"
 #include "libzerocoin/CoinSpend.h"
 #include "libzerocoin/Accumulator.h"
-#include "primitives/zerocoin.h"
-#include "primitives/deterministicmint.h"
-#include "zhlmwallet.h"
+#include "zhlm/zerocoin.h"
+#include "zhlm/deterministicmint.h"
+#include "zhlm/zhlmwallet.h"
 #include "libzerocoin/Coin.h"
 #include "amount.h"
 #include "chainparams.h"