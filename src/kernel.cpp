eons-finance/ALQO

[PoS] Compute old modifier, invalid integer division removal plus extra logging for the stake modifier height added.

src/kernel.cpp
@@ -167,7 +167,7 @@ bool ComputeNextStakeModifier(const CBlockIndex* pindexPrev, uint64_t& nStakeMod
     if (GetBoolArg("-printstakemodifier", false))
         LogPrintf("%s : prev modifier= %s time=%s\n", __func__, std::to_string(nStakeModifier).c_str(), DateTimeStrFormat("%Y-%m-%d %H:%M:%S", nModifierTime).c_str());
 
-    if (nModifierTime >= pindexPrev->GetBlockTime())
+    if (nModifierTime / MODIFIER_INTERVAL >= pindexPrev->GetBlockTime() / MODIFIER_INTERVAL)
         return true;
 
     // Sort candidate blocks by timestamp
@@ -327,7 +327,9 @@ bool GetHashProofOfStake(const CBlockIndex* pindexPrev, CStakeInput* stake, cons
     hashProofOfStakeRet = Hash(ss.begin(), ss.end());
 
     if (fDebug) {
-        LogPrintf("%s : nStakeModifier=%s\n", __func__, HexStr(modifier_ss));
+        LogPrintf("%s :{ nStakeModifier=%s\n"
+                  "nStakeModifierHeight=%s\n"
+                  "}\n", __func__, HexStr(modifier_ss), ((stake->IsZPIV()) ? "Not available" : std::to_string(stake->getStakeModifierHeight())));
     }
 
     return true;

heliumchain/squorum

src/kernel.cpp
@@ -175,7 +175,7 @@ bool ComputeNextStakeModifier(const CBlockIndex* pindexPrev, uint64_t& nStakeMod
     if (GetBoolArg("-printstakemodifier", false))
         LogPrintf("%s : prev modifier= %s time=%s\n", __func__, std::to_string(nStakeModifier).c_str(), DateTimeStrFormat("%Y-%m-%d %H:%M:%S", nModifierTime).c_str());
 
-    if (nModifierTime >= pindexPrev->GetBlockTime())
+    if (nModifierTime / MODIFIER_INTERVAL >= pindexPrev->GetBlockTime() / MODIFIER_INTERVAL)
         return true;
 
     // Sort candidate blocks by timestamp
@@ -335,7 +335,9 @@ bool GetHashProofOfStake(const CBlockIndex* pindexPrev, CStakeInput* stake, cons
     hashProofOfStakeRet = Hash(ss.begin(), ss.end());
 
     if (fDebug) {
-        LogPrintf("%s : nStakeModifier=%s\n", __func__, HexStr(modifier_ss));
+        LogPrintf("%s :{ nStakeModifier=%s\n"
+                  "nStakeModifierHeight=%s\n"
+                  "}\n", __func__, HexStr(modifier_ss), ((stake->IsZHLM()) ? "Not available" : std::to_string(stake->getStakeModifierHeight())));
     }
 
     return true;