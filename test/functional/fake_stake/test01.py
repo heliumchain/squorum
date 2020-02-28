eons-finance/ALQO

PIVX_fake_stake: fix txes in create_spam_block

test/functional/fake_stake/test01.py
@@ -1,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from random import randint
+import time
+
+from test_framework.messages import msg_block
+
+from base_test import PIVX_FakeStakeTest
+from util import utxos_to_stakingPrevOuts, dir_size
+
+class Test_01(PIVX_FakeStakeTest):
+
+    def run_test(self):
+        self.init_test()
+
+        FORK_DEPTH = 20  # Depth at which we are creating a fork. We are mining
+        INITAL_MINED_BLOCKS = 150
+        self.NUM_BLOCKS = 15
+
+        # 1) Starting mining blocks
+        self.log.info("Mining %d blocks.." % INITAL_MINED_BLOCKS)
+        self.node.generate(INITAL_MINED_BLOCKS)
+
+        # 2) Collect the possible prevouts
+        self.log.info("Collecting all unspent coins which we generated from mining...")
+
+        # 3) Create 10 addresses - Do the stake amplification
+        self.log.info("Performing the stake amplification (3 rounds)...")
+        utxo_list = self.node.listunspent()
+        address_list = []
+        for i in range(10):
+            address_list.append(self.node.getnewaddress())
+        utxo_list = self.stake_amplification(utxo_list, 3, address_list)
+
+        self.log.info("Done. Utxo list has %d elements." % len(utxo_list))
+        time.sleep(2)
+
+        # 4) collect the prevouts
+        self.log.info("Collecting inputs...")
+        tx_block_time = int(time.time())
+        stakingPrevOuts = utxos_to_stakingPrevOuts(utxo_list, tx_block_time)
+        time.sleep(1)
+
+        # 3) Start mining again so that spent prevouts get confirmted in a block.
+        self.log.info("Mining 5 more blocks...")
+        self.node.generate(5)
+        self.log.info("Sleeping 2 sec. Now mining PoS blocks based on already spent transactions...")
+        time.sleep(2)
+
+        # 4) Create "Fake Stake" blocks and send them
+        init_size = dir_size(self.node.datadir + "/regtest/blocks")
+        self.log.info("Initial size of data dir: %s kilobytes" % str(init_size))
+
+        for i in range(0, self.NUM_BLOCKS):
+            if i != 0 and i % 5 == 0:
+                self.log.info("Sent %s blocks out of %s" % (str(i), str(self.NUM_BLOCKS)))
+
+            # Create the spam block
+            block_count = self.node.getblockcount()
+            randomCount = randint(block_count-FORK_DEPTH-1, block_count)
+            pastBlockHash = self.node.getblockhash(randomCount)
+            block = self.create_spam_block(pastBlockHash, stakingPrevOuts)
+            timeStamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(block.nTime))
+            self.log.info("Created PoS block with nTime %s: %s", timeStamp, block.hash)
+            msg = msg_block(block)
+            self.log.info("Sending block (size: %.2f Kbytes)...", len(block.serialize())/1000)
+            self.test_nodes[0].send_message(msg)
+
+
+        self.log.info("Sent all %s blocks." % str(self.NUM_BLOCKS))
+        self.stop_node(0)
+        time.sleep(5)
+
+        final_size = dir_size(self.node.datadir + "/regtest/blocks")
+        self.log.info("Final size of data dir: %s kilobytes" % str(final_size))
+        self.log.info("Total size increase: %s kilobytes" % str(final_size-init_size))

test/functional/fake_stake/test02.py
@@ -1,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from random import randint
+import time
+
+from test_framework.messages import msg_block
+
+from base_test import PIVX_FakeStakeTest
+from util import utxos_to_stakingPrevOuts, dir_size
+
+class Test_02(PIVX_FakeStakeTest):
+
+    def run_test(self):
+        self.init_test()
+        INITAL_MINED_BLOCKS = 150
+        MORE_MINED_BLOCKS = 50
+        self.NUM_BLOCKS = 3
+
+        # 1) Starting mining blocks
+        self.log.info("Mining %d blocks.." % INITAL_MINED_BLOCKS)
+        self.node.generate(INITAL_MINED_BLOCKS)
+
+        # 2) Collect the possible prevouts
+        self.log.info("Collecting all unspent coins which we generated from mining...")
+        utxo_list = self.node.listunspent()
+        time.sleep(2)
+
+        # 3) Mine more blocks
+        self.log.info("Mining %d more blocks.." % MORE_MINED_BLOCKS)
+        self.node.generate(MORE_MINED_BLOCKS)
+        time.sleep(2)
+
+        # 4) Spend the coins collected in 2 (mined in the first 100 blocks)
+        self.log.info("Spending the coins mined in the first %d blocks..." % INITAL_MINED_BLOCKS)
+        tx_block_time = int(time.time())
+        stakingPrevOuts = utxos_to_stakingPrevOuts(utxo_list, tx_block_time)
+        tx_hashes = self.spend_utxos(utxo_list)
+        self.log.info("Spent %d transactions" % len(tx_hashes))
+        time.sleep(2)
+
+        # 5) Mine 10 more blocks
+        self.log.info("Mining 10 more blocks to include the TXs in chain...")
+        self.node.generate(10)
+        time.sleep(2)
+
+        # 6) Create PoS blocks and send them
+        init_size = dir_size(self.node.datadir + "/regtest/blocks")
+        self.log.info("Initial size of data dir: %s kilobytes" % str(init_size))
+
+        for i in range(0, self.NUM_BLOCKS):
+            if i != 0 and i % 5 == 0:
+                self.log.info("Sent %s blocks out of %s" % (str(i), str(self.NUM_BLOCKS)))
+
+            # Create the spam block
+            block_count = self.node.getblockcount()
+            randomCount = randint(block_count-MORE_MINED_BLOCKS-1, block_count-12)
+            pastBlockHash = self.node.getblockhash(randomCount)
+            block = self.create_spam_block(pastBlockHash, stakingPrevOuts)
+            timeStamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(block.nTime))
+            self.log.info("Created PoS block with nTime %s: %s", timeStamp, block.hash)
+            msg = msg_block(block)
+            self.log.info("Sending block (size: %.2f Kbytes)...", len(block.serialize())/1000)
+            self.test_nodes[0].send_message(msg)
+
+
+        self.log.info("Sent all %s blocks." % str(self.NUM_BLOCKS))
+        self.stop_node(0)
+        time.sleep(5)
+
+        final_size = dir_size(self.node.datadir + "/regtest/blocks")
+        self.log.info("Final size of data dir: %s kilobytes" % str(final_size))
+        self.log.info("Total size increase: %s kilobytes" % str(final_size-init_size))
+

test/functional/fake_stake/util.py
@@ -1,0 +1,129 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+from test_framework.messages import CTransaction, CTxIn, CTxOut, COutPoint, COIN
+from test_framework.messages import msg_getheaders, msg_headers, CBlockHeader
+from test_framework.mininode import P2PInterface, mininode_lock
+from test_framework.script import CScript
+from test_framework.util import wait_until
+
+''' -------------------------------------------------------------------------
+TestNode CLASS --------------------------------------------------------------
+
+A peer we use to send messsages to pivxd and store responses
+Extends P2PInterface.
+'''
+
+# TestNode: A peer we use to send messages to bitcoind, and store responses.
+class TestNode(P2PInterface):
+    def __init__(self):
+        super().__init__()
+        self.last_sendcmpct = []
+        self.block_announced = False
+        # Store the hashes of blocks we've seen announced.
+        # This is for synchronizing the p2p message traffic,
+        # so we can eg wait until a particular block is announced.
+        self.announced_blockhashes = set()
+
+    def on_sendcmpct(self, message):
+        self.last_sendcmpct.append(message)
+
+    def on_cmpctblock(self, message):
+        self.block_announced = True
+        self.last_message["cmpctblock"].header_and_shortids.header.calc_sha256()
+        self.announced_blockhashes.add(self.last_message["cmpctblock"].header_and_shortids.header.sha256)
+
+    def on_headers(self, message):
+        self.block_announced = True
+        for x in self.last_message["headers"].headers:
+            x.calc_sha256()
+            self.announced_blockhashes.add(x.sha256)
+
+    def on_inv(self, message):
+        for x in self.last_message["inv"].inv:
+            if x.type == 2:
+                self.block_announced = True
+                self.announced_blockhashes.add(x.hash)
+
+    # Requires caller to hold mininode_lock
+    def received_block_announcement(self):
+        return self.block_announced
+
+    def clear_block_announcement(self):
+        with mininode_lock:
+            self.block_announced = False
+            self.last_message.pop("inv", None)
+            self.last_message.pop("headers", None)
+            self.last_message.pop("cmpctblock", None)
+
+    def get_headers(self, locator, hashstop):
+        msg = msg_getheaders()
+        msg.locator.vHave = locator
+        msg.hashstop = hashstop
+        self.connection.send_message(msg)
+
+    def send_header_for_blocks(self, new_blocks):
+        headers_message = msg_headers()
+        headers_message.headers = [CBlockHeader(b) for b in new_blocks]
+        self.send_message(headers_message)
+
+    def request_headers_and_sync(self, locator, hashstop=0):
+        self.clear_block_announcement()
+        self.get_headers(locator, hashstop)
+        wait_until(self.received_block_announcement, timeout=30, lock=mininode_lock)
+        self.clear_block_announcement()
+
+    # Block until a block announcement for a particular block hash is received.
+    def wait_for_block_announcement(self, block_hash, timeout=30):
+        def received_hash():
+            return (block_hash in self.announced_blockhashes)
+        wait_until(received_hash, timeout=timeout, lock=mininode_lock)
+
+    def send_await_disconnect(self, message, timeout=30):
+        """Sends a message to the node and wait for disconnect.
+
+        This is used when we want to send a message into the node that we expect
+        will get us disconnected, eg an invalid block."""
+        self.send_message(message)
+        wait_until(lambda: not self.connected, timeout=timeout, lock=mininode_lock)
+
+
+
+''' -------------------------------------------------------------------------
+MISC METHODS ----------------------------------------------------------------
+'''
+
+def dir_size(path):
+    ''' returns the size in bytes of the directory at given path
+    '''
+    size = subprocess.check_output(['du','-shk', path]).split()[0].decode('utf-8')
+    return int(size)
+
+
+def create_transaction(outPoint, sig, value, nTime, scriptPubKey=CScript()):
+    ''' creates a CTransaction object provided input-output data
+    '''
+    tx = CTransaction()
+    tx.vin.append(CTxIn(outPoint, sig, 0xffffffff))
+    tx.vout.append(CTxOut(value, scriptPubKey))
+    tx.nTime = nTime
+    tx.calc_sha256()
+    return tx
+
+
+def utxos_to_stakingPrevOuts(utxolist, blocktime):
+    '''
+    Creates a map of unspent outputs to (amount, blocktime) to be used as stake inputs
+    :param   utxolist:          list of JSON object returned from listunspent
+    :return  stakingPrevOuts:   ({COutPoint --> (int, int)} dictionary)
+    '''
+    stakingPrevOuts = {}
+    COINBASE_MATURITY = 100
+    for utxo in utxolist:
+        if utxo['confirmations'] > COINBASE_MATURITY:
+            outPoint = COutPoint(int(utxo['txid'], 16), utxo['vout'])
+            stakingPrevOuts[outPoint] = (int(utxo['amount'])*COIN, blocktime)
+
+    return stakingPrevOuts

heliumchain/squorum

test/functional/fake_stake/test01.py
@@ -1,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from random import randint
+import time
+
+from test_framework.messages import msg_block
+
+from base_test import Helium_FakeStakeTest
+from util import utxos_to_stakingPrevOuts, dir_size
+
+class Test_01(Helium_FakeStakeTest):
+
+    def run_test(self):
+        self.init_test()
+
+        FORK_DEPTH = 20  # Depth at which we are creating a fork. We are mining
+        INITAL_MINED_BLOCKS = 150
+        self.NUM_BLOCKS = 15
+
+        # 1) Starting mining blocks
+        self.log.info("Mining %d blocks.." % INITAL_MINED_BLOCKS)
+        self.node.generate(INITAL_MINED_BLOCKS)
+
+        # 2) Collect the possible prevouts
+        self.log.info("Collecting all unspent coins which we generated from mining...")
+
+        # 3) Create 10 addresses - Do the stake amplification
+        self.log.info("Performing the stake amplification (3 rounds)...")
+        utxo_list = self.node.listunspent()
+        address_list = []
+        for i in range(10):
+            address_list.append(self.node.getnewaddress())
+        utxo_list = self.stake_amplification(utxo_list, 3, address_list)
+
+        self.log.info("Done. Utxo list has %d elements." % len(utxo_list))
+        time.sleep(2)
+
+        # 4) collect the prevouts
+        self.log.info("Collecting inputs...")
+        tx_block_time = int(time.time())
+        stakingPrevOuts = utxos_to_stakingPrevOuts(utxo_list, tx_block_time)
+        time.sleep(1)
+
+        # 3) Start mining again so that spent prevouts get confirmted in a block.
+        self.log.info("Mining 5 more blocks...")
+        self.node.generate(5)
+        self.log.info("Sleeping 2 sec. Now mining PoS blocks based on already spent transactions...")
+        time.sleep(2)
+
+        # 4) Create "Fake Stake" blocks and send them
+        init_size = dir_size(self.node.datadir + "/regtest/blocks")
+        self.log.info("Initial size of data dir: %s kilobytes" % str(init_size))
+
+        for i in range(0, self.NUM_BLOCKS):
+            if i != 0 and i % 5 == 0:
+                self.log.info("Sent %s blocks out of %s" % (str(i), str(self.NUM_BLOCKS)))
+
+            # Create the spam block
+            block_count = self.node.getblockcount()
+            randomCount = randint(block_count-FORK_DEPTH-1, block_count)
+            pastBlockHash = self.node.getblockhash(randomCount)
+            block = self.create_spam_block(pastBlockHash, stakingPrevOuts)
+            timeStamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(block.nTime))
+            self.log.info("Created PoS block with nTime %s: %s", timeStamp, block.hash)
+            msg = msg_block(block)
+            self.log.info("Sending block (size: %.2f Kbytes)...", len(block.serialize())/1000)
+            self.test_nodes[0].send_message(msg)
+
+
+        self.log.info("Sent all %s blocks." % str(self.NUM_BLOCKS))
+        self.stop_node(0)
+        time.sleep(5)
+
+        final_size = dir_size(self.node.datadir + "/regtest/blocks")
+        self.log.info("Final size of data dir: %s kilobytes" % str(final_size))
+        self.log.info("Total size increase: %s kilobytes" % str(final_size-init_size))

test/functional/fake_stake/test02.py
@@ -1,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from random import randint
+import time
+
+from test_framework.messages import msg_block
+
+from base_test import Helium_FakeStakeTest
+from util import utxos_to_stakingPrevOuts, dir_size
+
+class Test_02(Helium_FakeStakeTest):
+
+    def run_test(self):
+        self.init_test()
+        INITAL_MINED_BLOCKS = 150
+        MORE_MINED_BLOCKS = 50
+        self.NUM_BLOCKS = 3
+
+        # 1) Starting mining blocks
+        self.log.info("Mining %d blocks.." % INITAL_MINED_BLOCKS)
+        self.node.generate(INITAL_MINED_BLOCKS)
+
+        # 2) Collect the possible prevouts
+        self.log.info("Collecting all unspent coins which we generated from mining...")
+        utxo_list = self.node.listunspent()
+        time.sleep(2)
+
+        # 3) Mine more blocks
+        self.log.info("Mining %d more blocks.." % MORE_MINED_BLOCKS)
+        self.node.generate(MORE_MINED_BLOCKS)
+        time.sleep(2)
+
+        # 4) Spend the coins collected in 2 (mined in the first 100 blocks)
+        self.log.info("Spending the coins mined in the first %d blocks..." % INITAL_MINED_BLOCKS)
+        tx_block_time = int(time.time())
+        stakingPrevOuts = utxos_to_stakingPrevOuts(utxo_list, tx_block_time)
+        tx_hashes = self.spend_utxos(utxo_list)
+        self.log.info("Spent %d transactions" % len(tx_hashes))
+        time.sleep(2)
+
+        # 5) Mine 10 more blocks
+        self.log.info("Mining 10 more blocks to include the TXs in chain...")
+        self.node.generate(10)
+        time.sleep(2)
+
+        # 6) Create PoS blocks and send them
+        init_size = dir_size(self.node.datadir + "/regtest/blocks")
+        self.log.info("Initial size of data dir: %s kilobytes" % str(init_size))
+
+        for i in range(0, self.NUM_BLOCKS):
+            if i != 0 and i % 5 == 0:
+                self.log.info("Sent %s blocks out of %s" % (str(i), str(self.NUM_BLOCKS)))
+
+            # Create the spam block
+            block_count = self.node.getblockcount()
+            randomCount = randint(block_count-MORE_MINED_BLOCKS-1, block_count-12)
+            pastBlockHash = self.node.getblockhash(randomCount)
+            block = self.create_spam_block(pastBlockHash, stakingPrevOuts)
+            timeStamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(block.nTime))
+            self.log.info("Created PoS block with nTime %s: %s", timeStamp, block.hash)
+            msg = msg_block(block)
+            self.log.info("Sending block (size: %.2f Kbytes)...", len(block.serialize())/1000)
+            self.test_nodes[0].send_message(msg)
+
+
+        self.log.info("Sent all %s blocks." % str(self.NUM_BLOCKS))
+        self.stop_node(0)
+        time.sleep(5)
+
+        final_size = dir_size(self.node.datadir + "/regtest/blocks")
+        self.log.info("Final size of data dir: %s kilobytes" % str(final_size))
+        self.log.info("Total size increase: %s kilobytes" % str(final_size-init_size))
+

test/functional/fake_stake/util.py
@@ -1,0 +1,129 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+from test_framework.messages import CTransaction, CTxIn, CTxOut, COutPoint, COIN
+from test_framework.messages import msg_getheaders, msg_headers, CBlockHeader
+from test_framework.mininode import P2PInterface, mininode_lock
+from test_framework.script import CScript
+from test_framework.util import wait_until
+
+''' -------------------------------------------------------------------------
+TestNode CLASS --------------------------------------------------------------
+
+A peer we use to send messsages to heliumd and store responses
+Extends P2PInterface.
+'''
+
+# TestNode: A peer we use to send messages to bitcoind, and store responses.
+class TestNode(P2PInterface):
+    def __init__(self):
+        super().__init__()
+        self.last_sendcmpct = []
+        self.block_announced = False
+        # Store the hashes of blocks we've seen announced.
+        # This is for synchronizing the p2p message traffic,
+        # so we can eg wait until a particular block is announced.
+        self.announced_blockhashes = set()
+
+    def on_sendcmpct(self, message):
+        self.last_sendcmpct.append(message)
+
+    def on_cmpctblock(self, message):
+        self.block_announced = True
+        self.last_message["cmpctblock"].header_and_shortids.header.calc_sha256()
+        self.announced_blockhashes.add(self.last_message["cmpctblock"].header_and_shortids.header.sha256)
+
+    def on_headers(self, message):
+        self.block_announced = True
+        for x in self.last_message["headers"].headers:
+            x.calc_sha256()
+            self.announced_blockhashes.add(x.sha256)
+
+    def on_inv(self, message):
+        for x in self.last_message["inv"].inv:
+            if x.type == 2:
+                self.block_announced = True
+                self.announced_blockhashes.add(x.hash)
+
+    # Requires caller to hold mininode_lock
+    def received_block_announcement(self):
+        return self.block_announced
+
+    def clear_block_announcement(self):
+        with mininode_lock:
+            self.block_announced = False
+            self.last_message.pop("inv", None)
+            self.last_message.pop("headers", None)
+            self.last_message.pop("cmpctblock", None)
+
+    def get_headers(self, locator, hashstop):
+        msg = msg_getheaders()
+        msg.locator.vHave = locator
+        msg.hashstop = hashstop
+        self.connection.send_message(msg)
+
+    def send_header_for_blocks(self, new_blocks):
+        headers_message = msg_headers()
+        headers_message.headers = [CBlockHeader(b) for b in new_blocks]
+        self.send_message(headers_message)
+
+    def request_headers_and_sync(self, locator, hashstop=0):
+        self.clear_block_announcement()
+        self.get_headers(locator, hashstop)
+        wait_until(self.received_block_announcement, timeout=30, lock=mininode_lock)
+        self.clear_block_announcement()
+
+    # Block until a block announcement for a particular block hash is received.
+    def wait_for_block_announcement(self, block_hash, timeout=30):
+        def received_hash():
+            return (block_hash in self.announced_blockhashes)
+        wait_until(received_hash, timeout=timeout, lock=mininode_lock)
+
+    def send_await_disconnect(self, message, timeout=30):
+        """Sends a message to the node and wait for disconnect.
+
+        This is used when we want to send a message into the node that we expect
+        will get us disconnected, eg an invalid block."""
+        self.send_message(message)
+        wait_until(lambda: not self.connected, timeout=timeout, lock=mininode_lock)
+
+
+
+''' -------------------------------------------------------------------------
+MISC METHODS ----------------------------------------------------------------
+'''
+
+def dir_size(path):
+    ''' returns the size in bytes of the directory at given path
+    '''
+    size = subprocess.check_output(['du','-shk', path]).split()[0].decode('utf-8')
+    return int(size)
+
+
+def create_transaction(outPoint, sig, value, nTime, scriptPubKey=CScript()):
+    ''' creates a CTransaction object provided input-output data
+    '''
+    tx = CTransaction()
+    tx.vin.append(CTxIn(outPoint, sig, 0xffffffff))
+    tx.vout.append(CTxOut(value, scriptPubKey))
+    tx.nTime = nTime
+    tx.calc_sha256()
+    return tx
+
+
+def utxos_to_stakingPrevOuts(utxolist, blocktime):
+    '''
+    Creates a map of unspent outputs to (amount, blocktime) to be used as stake inputs
+    :param   utxolist:          list of JSON object returned from listunspent
+    :return  stakingPrevOuts:   ({COutPoint --> (int, int)} dictionary)
+    '''
+    stakingPrevOuts = {}
+    COINBASE_MATURITY = 100
+    for utxo in utxolist:
+        if utxo['confirmations'] > COINBASE_MATURITY:
+            outPoint = COutPoint(int(utxo['txid'], 16), utxo['vout'])
+            stakingPrevOuts[outPoint] = (int(utxo['amount'])*COIN, blocktime)
+
+    return stakingPrevOuts