furszy/PIVX-1

Add global boolean, that stop precompute cache lock

src/rpcwallet.cpp
@@ -3684,7 +3684,7 @@ UniValue clearspendcache(const UniValue& params, bool fHelp)
     if(fHelp || params.size() != 0)
         throw runtime_error(
             "clearspendcache\n"
-            "\nClear the pre-computed zPIV spend cache.\n" +
+            "\nClear the pre-computed zPIV spend cache, and database.\n" +
             HelpRequiringPassphrase() + "\n"
 
             "\nExamples\n" +
@@ -3695,13 +3695,20 @@ UniValue clearspendcache(const UniValue& params, bool fHelp)
     CzPIVTracker* zpivTracker = pwalletMain->zpivTracker.get();
 
     {
-        TRY_LOCK(zpivTracker->cs_spendcache, fLocked);
-        if (fLocked) {
-            if (zpivTracker->ClearSpendCache()) {
-                fClearSpendCache = true;
-                CWalletDB walletdb("precomputes.dat", "cr+");
-                walletdb.EraseAllPrecomputes();
-                return NullUniValue;
+        int nTries = 0;
+        while (nTries < 100) {
+            TRY_LOCK(zpivTracker->cs_spendcache, fLocked);
+            if (fLocked) {
+                if (zpivTracker->ClearSpendCache()) {
+                    fClearSpendCache = true;
+                    CWalletDB walletdb("precomputes.dat", "cr+");
+                    walletdb.EraseAllPrecomputes();
+                    return "Successfully Cleared the Precompute Spend Cache and Database";
+                }
+            } else {
+                fGlobalUnlockSpendCache = true;
+                nTries++;
+                MilliSleep(100);
             }
         }
     }

src/wallet.h
@@ -45,6 +45,7 @@ extern bool bSpendZeroConfChange;
 extern bool bdisableSystemnotifications;
 extern bool fSendFreeTransactions;
 extern bool fPayAtLeastCustomFee;
+extern bool fGlobalUnlockSpendCache; // Bool used for letting the precomputing thread know that zpivspends need to use the cs_spendcache
 
 //! -paytxfee default
 static const CAmount DEFAULT_TRANSACTION_FEE = 0;

heliumchain/squorum

src/rpcwallet.cpp
@@ -3763,7 +3763,7 @@ UniValue clearspendcache(const UniValue& params, bool fHelp)
     if(fHelp || params.size() != 0)
         throw runtime_error(
             "clearspendcache\n"
-            "\nClear the pre-computed zHLM spend cache.\n" +
+            "\nClear the pre-computed zHLM spend cache, and database.\n" +
             HelpRequiringPassphrase() + "\n"
 
             "\nExamples\n" +
@@ -3774,13 +3774,20 @@ UniValue clearspendcache(const UniValue& params, bool fHelp)
     CzHLMTracker* zhlmTracker = pwalletMain->zhlmTracker.get();
 
     {
-        TRY_LOCK(zhlmTracker->cs_spendcache, fLocked);
-        if (fLocked) {
-            if (zhlmTracker->ClearSpendCache()) {
-                fClearSpendCache = true;
-                CWalletDB walletdb("precomputes.dat", "cr+");
-                walletdb.EraseAllPrecomputes();
-                return NullUniValue;
+        int nTries = 0;
+        while (nTries < 100) {
+            TRY_LOCK(zhlmTracker->cs_spendcache, fLocked);
+            if (fLocked) {
+                if (zhlmTracker->ClearSpendCache()) {
+                    fClearSpendCache = true;
+                    CWalletDB walletdb("precomputes.dat", "cr+");
+                    walletdb.EraseAllPrecomputes();
+                    return "Successfully Cleared the Precompute Spend Cache and Database";
+                }
+            } else {
+                fGlobalUnlockSpendCache = true;
+                nTries++;
+                MilliSleep(100);
             }
         }
     }

src/wallet.h
@@ -47,6 +47,7 @@ extern bool bSpendZeroConfChange;
 extern bool bdisableSystemnotifications;
 extern bool fSendFreeTransactions;
 extern bool fPayAtLeastCustomFee;
+extern bool fGlobalUnlockSpendCache; // Bool used for letting the precomputing thread know that zhlmspends need to use the cs_spendcache
 
 //! -paytxfee default
 static const CAmount DEFAULT_TRANSACTION_FEE = 0;