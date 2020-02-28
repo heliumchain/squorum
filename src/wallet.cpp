furszy/PIVX-1

Convert precompute cache into LRU cache

src/wallet.cpp
@@ -5476,10 +5476,18 @@ void CWallet::PrecomputeSpends()
     LogPrintf("Precomputer started\n");
     RenameThread("pivx-precomputer");
 
+    CWalletDB walletdb("precomputes.dat", "cr+");
+
+    // Create LRU Cache
+    std::list<std::pair<uint256, CoinWitnessCacheData> > item_list;
+    std::map<uint256, list<std::pair<uint256, CoinWitnessCacheData> >::iterator> item_map;
+
+    // Dirty cache that needs to be written to disk
+    std::map<uint256, CoinWitnessCacheData> mapDirtyWitnessData;
+
     // Initialize Variables
     bool fLoadedPrecomputesFromDB = false;
     bool fOnFirstLoad = true;
-    std::map<uint256, CoinWitnessCacheData> mapPrecomputeCache;
     int64_t nLastCacheCleanUpTime = GetTime();
     int64_t nLastCacheWriteDB = nLastCacheCleanUpTime;
     int nRequiredStakeDepthBuffer = Params().Zerocoin_RequiredStakeDepth() + 10;
@@ -5496,17 +5504,18 @@ void CWallet::PrecomputeSpends()
         nAdjustableCacheLength = MAX_PRECOMPUTE_LENGTH;
 
     while (true) {
-
         // Check to see if we need to clear the cache
         if (fClearSpendCache) {
             fClearSpendCache = false;
-            mapPrecomputeCache.clear();
+            item_map.clear();
+            item_list.clear();
+            mapDirtyWitnessData.clear();
             nLastCacheCleanUpTime = GetTime();
             nLastCacheWriteDB = nLastCacheCleanUpTime;
         }
 
         // Get the list of zPIV inputs
-        std::list<std::unique_ptr<CStakeInput> > listInputs;
+        std::list <std::unique_ptr<CStakeInput>> listInputs;
         if (!SelectStakeCoins(listInputs, 0, true)) {
             MilliSleep(5000);
             continue;
@@ -5525,17 +5534,20 @@ void CWallet::PrecomputeSpends()
 
         // If we haven't loaded from database yet, load the precomputes from the database
         if (!fLoadedPrecomputesFromDB) {
-            CWalletDB walletdb("precomputes.dat", "cr+");
-            walletdb.ReadPrecomputes(mapPrecomputeCache);
+            // Load the precomputes into the LRU cache
+            walletdb.LoadPrecomputes(item_list, item_map);
             fLoadedPrecomputesFromDB = true;
-            LogPrint("precompute", "%s: Loaded precomputes from database. Size of map: %d\n", __func__, mapPrecomputeCache.size());
+            LogPrint("precompute", "%s: Loaded precomputes from database. Size of lru cache: %d\n", __func__,
+                     item_map.size());
         }
 
         // Do some precomputing of zerocoin spend knowledge proofs
-        std::set<uint256> setInputHashes;
-        for (std::unique_ptr<CStakeInput>& stakeInput : listInputs) {
+        std::set <uint256> setInputHashes;
+        for (std::unique_ptr <CStakeInput>& stakeInput : listInputs) {
             if (ShutdownRequested() || IsLocked())
                 break;
+
+            CoinWitnessCacheData tempDataHolder;
 
             {
                 TRY_LOCK(zpivTracker->cs_spendcache, fLocked);
@@ -5547,10 +5559,10 @@ void CWallet::PrecomputeSpends()
                 if (fClearSpendCache) {
                     break;
                 }
-                
+
                 uint256 serialHash = stakeInput->GetSerialHash();
                 setInputHashes.insert(serialHash);
-                CoinWitnessData *witnessData = zpivTracker->GetSpendCache(serialHash);
+                CoinWitnessData* witnessData = zpivTracker->GetSpendCache(serialHash);
 
                 // Initialize nHeightStop so it can be set below
                 int nHeightStop = 0;
@@ -5558,10 +5570,14 @@ void CWallet::PrecomputeSpends()
                 if (witnessData->nHeightAccStart) { // Witness is already valid
                     nHeightStop = std::min(chainActive.Height() - nRequiredStakeDepthBuffer,
                                            (witnessData->nHeightAccEnd ? witnessData->nHeightAccEnd
-                                                                       : witnessData->nHeightAccStart) + nAdjustableCacheLength);
-                } else if (mapPrecomputeCache.count(serialHash)) { // Check Database cache
+                                                                       : witnessData->nHeightAccStart) +
+                                           nAdjustableCacheLength);
+                } else if (item_map.count(serialHash)) { // Check Database cache
                     // Get the witness data from the cache
-                    *witnessData = CoinWitnessData(mapPrecomputeCache.at(serialHash));
+                    auto it = item_map.find(serialHash);
+                    item_list.splice(item_list.begin(), item_list, it->second);
+
+                    *witnessData = CoinWitnessData(it->second->second);
 
                     // Set the stop height from the variables received from the database cache
                     nHeightStop = std::min(chainActive.Height() - nRequiredStakeDepthBuffer,
@@ -5569,66 +5585,148 @@ void CWallet::PrecomputeSpends()
                                                                        : witnessData->nHeightAccStart) +
                                            nAdjustableCacheLength);
 
-                    LogPrint("precompute", "%s: Got Witness Data from Database: %s\n", __func__, witnessData->ToString());
+                    LogPrint("precompute", "%s: Got Witness Data from lru cache: %s\n", __func__,
+                             witnessData->ToString());
+                } else if (mapDirtyWitnessData.count(serialHash) || walletdb.ReadPrecompute(serialHash, tempDataHolder)) {
+                    if (mapDirtyWitnessData.count(serialHash)) {
+                        // Get the witness data from the dirty cache if it exists
+                        *witnessData = CoinWitnessData(mapDirtyWitnessData.at(serialHash));
+                        LogPrint("precompute", "%s: Got Witness Data from mapDirtyWitnessData: %s\n", __func__,
+                                 witnessData->ToString());
+                    } else {
+                        // Get the witness data from the database
+                        *witnessData = CoinWitnessData(tempDataHolder);
+                        LogPrint("precompute", "%s: Got Witness Data from precompute database: %s\n", __func__,
+                                 witnessData->ToString());
+                    }
+
+                    // Set the stop height from the variables received from the database cache
+                    nHeightStop = std::min(chainActive.Height() - nRequiredStakeDepthBuffer,
+                                           (witnessData->nHeightAccEnd ? witnessData->nHeightAccEnd
+                                                                       : witnessData->nHeightAccStart) +
+                                           nAdjustableCacheLength);
+
+                    // Add the serialHash found into the cache
+                    item_list.push_front(make_pair(serialHash, tempDataHolder));
+                    item_map.insert(make_pair(serialHash, item_list.begin()));
+
+                    // We just added a new hash into our LRU cache, so remove it if we also have it in the dirty map
+                    mapDirtyWitnessData.erase(serialHash);
+
+                    if (item_map.size() > PRECOMPUTE_LRU_CACHE_SIZE) {
+                        auto last_it = item_list.end(); last_it --;
+                        item_map.erase(last_it->first);
+                        CoinWitnessCacheData removedData = item_list.back().second;
+                        mapDirtyWitnessData[serialHash] = removedData;
+                        item_list.pop_back();
+                    }
                 } else { // This has no cache, so initialize it
                     CZerocoinMint mint;
                     if (!GetMintFromStakeHash(serialHash, mint))
                         continue;
                     *witnessData = CoinWitnessData(mint);
-                    nHeightStop = std::min(chainActive.Height() - nRequiredStakeDepthBuffer, mint.GetHeight() + nAdjustableCacheLength);
+                    nHeightStop = std::min(chainActive.Height() - nRequiredStakeDepthBuffer,
+                                           mint.GetHeight() + nAdjustableCacheLength);
                 }
 
                 if (nHeightStop - (witnessData->nHeightAccEnd ? witnessData->nHeightAccEnd : witnessData->nHeightAccStart) < 20)
                     continue;
 
-                CBlockIndex *pindexStop = chainActive[nHeightStop];
+                CBlockIndex* pindexStop = chainActive[nHeightStop];
                 AccumulatorMap mapAccumulators(Params().Zerocoin_Params(false));
-                LogPrintf("%s: caching mint %s of denom %d start=%d stop=%d end=%s\n", __func__,
+                LogPrint("precompute", "%s: caching mint %s of denom %d start=%d stop=%d end=%s\n", __func__,
                           witnessData->coin->getValue().GetHex().substr(0, 6),
                           ZerocoinDenominationToInt(witnessData->denom),
                           witnessData->nHeightAccStart, nHeightStop, witnessData->nHeightAccEnd);
 
                 if (!GenerateAccumulatorWitness(witnessData, mapAccumulators, pindexStop)) {
-                    LogPrintf("%s: caching of witness failed!\n", __func__);
+                    LogPrintf("%s: Generate witness failed!\n", __func__);
 
-                    // If we fail this check, we need to make sure we remove this from the pre compute cache
-                    mapPrecomputeCache.erase(serialHash);
+                    // If we fail this check, we need to make sure we remove this from the LRU cache
+                    auto it = item_map.find(serialHash);
+                    if (it != item_map.end())
+                    {
+                        item_list.erase(it->second);
+                        item_map.erase(it);
+                    }
+                    mapDirtyWitnessData.erase(serialHash);
+                    walletdb.ErasePrecompute(serialHash);
                     continue;
                 }
 
-                // Update the database cache with the new witness data
+
                 CoinWitnessCacheData serialData(witnessData);
-                mapPrecomputeCache[serialHash] = serialData;
+
+                // If the LRU cache already has a entry for it, update the entry and move it to the front of the list
+                auto it = item_map.find(serialHash);
+                if (it != item_map.end()) {
+                    item_list.splice(item_list.begin(), item_list, it->second);
+                    item_list.begin()->second = serialData;
+                } else {
+                    item_list.push_front(make_pair(serialHash, serialData));
+                    item_map.insert(make_pair(serialHash, item_list.begin()));
+                }
+
+                // We just added a new hash into our LRU cache, so remove it if we also have it in the dirty map
+                mapDirtyWitnessData.erase(serialHash);
+
+                // Clean up the LRU cache to the max size
+                while (item_map.size() > PRECOMPUTE_LRU_CACHE_SIZE) {
+                    auto last_it = item_list.end(); last_it --;
+                    item_map.erase(last_it->first);
+                    mapDirtyWitnessData[serialHash] = item_list.back().second;
+                    item_list.pop_back();
+                }
             }
             // Sleep for 150ms to allow any potential spend attempt
             MilliSleep(150);
         }
 
         // On first load, and every 5 minutes clean up our cache with only valid unspent inputs
-        if (fOnFirstLoad || nLastCacheCleanUpTime < GetTime() - 300) {
+        if (fOnFirstLoad || nLastCacheCleanUpTime < GetTime() - PRECOMPUTE_FLUSH_TIME) {
             LogPrint("precompute", "%s: Cleaning up precompute cache\n", __func__);
 
             // We only want to clear the cache if we have calculated new witness data
-           if (setInputHashes.size()) {
-                // Create temporary cache to hold witness data
-                std::map<uint256, CoinWitnessCacheData> mapLatestCoinWitnessData;
+            if (setInputHashes.size()) {
+                // Get a list of hashes currently in the database
+                set <uint256> databaseHashes;
+                walletdb.LoadPrecomputes(databaseHashes);
 
                 // Remove old cache data
                 for (auto inputHash : setInputHashes) {
-                    if (mapPrecomputeCache.count(inputHash))
-                        mapLatestCoinWitnessData.insert(make_pair(inputHash, mapPrecomputeCache.at(inputHash)));
+                    databaseHashes.erase(inputHash);
                 }
-                mapPrecomputeCache = mapLatestCoinWitnessData;
+
+                // Erase all old hashes from the database
+                for (auto hash : databaseHashes) {
+                    auto it = item_map.find(hash);
+                    if (it != item_map.end())
+                    {
+                        item_list.erase(it->second);
+                        item_map.erase(it);
+                    }
+                    mapDirtyWitnessData.erase(hash);
+                    walletdb.ErasePrecompute(hash);
+                }
+
                 nLastCacheCleanUpTime = GetTime();
             }
         }
 
         // On first load, and every 5 minutes write the cache to database
-        if (fOnFirstLoad || nLastCacheWriteDB < GetTime() - 300 || ShutdownRequested()) {
-            CWalletDB walletdb("precomputes.dat", "cr+");
-            walletdb.WritePrecomputes(mapPrecomputeCache);
+        if (mapDirtyWitnessData.size() > PRECOMPUTE_MAX_DIRTY_CACHE_SIZE || nLastCacheWriteDB < GetTime() - PRECOMPUTE_FLUSH_TIME || ShutdownRequested()) {
+            // Save all cache data that was dirty back into the database
+            for (auto item : mapDirtyWitnessData) {
+                walletdb.WritePrecompute(item.first, item.second);
+            }
+            mapDirtyWitnessData.clear();
 
-            LogPrint("precompute", "%s: Writing precomputes to database. Precomputes size: %d\n", __func__, mapPrecomputeCache.size());
+            // Save the LRU cache data into the database
+            for (auto item : item_list) {
+                walletdb.WritePrecompute(item.first, item.second);
+            }
+
+            LogPrint("precompute", "%s: Writing precomputes to database. Precomputes size: %d\n", __func__, item_map.size());
             nLastCacheWriteDB = GetTime();
         }
 

heliumchain/squorum

src/wallet.cpp
@@ -5504,10 +5504,18 @@ void CWallet::PrecomputeSpends()
     LogPrintf("Precomputer started\n");
     RenameThread("helium-precomputer");
 
+    CWalletDB walletdb("precomputes.dat", "cr+");
+
+    // Create LRU Cache
+    std::list<std::pair<uint256, CoinWitnessCacheData> > item_list;
+    std::map<uint256, list<std::pair<uint256, CoinWitnessCacheData> >::iterator> item_map;
+
+    // Dirty cache that needs to be written to disk
+    std::map<uint256, CoinWitnessCacheData> mapDirtyWitnessData;
+
     // Initialize Variables
     bool fLoadedPrecomputesFromDB = false;
     bool fOnFirstLoad = true;
-    std::map<uint256, CoinWitnessCacheData> mapPrecomputeCache;
     int64_t nLastCacheCleanUpTime = GetTime();
     int64_t nLastCacheWriteDB = nLastCacheCleanUpTime;
     int nRequiredStakeDepthBuffer = Params().Zerocoin_RequiredStakeDepth() + 10;
@@ -5524,17 +5532,18 @@ void CWallet::PrecomputeSpends()
         nAdjustableCacheLength = MAX_PRECOMPUTE_LENGTH;
 
     while (true) {
-
         // Check to see if we need to clear the cache
         if (fClearSpendCache) {
             fClearSpendCache = false;
-            mapPrecomputeCache.clear();
+            item_map.clear();
+            item_list.clear();
+            mapDirtyWitnessData.clear();
             nLastCacheCleanUpTime = GetTime();
             nLastCacheWriteDB = nLastCacheCleanUpTime;
         }
 
         // Get the list of zHLM inputs
-        std::list<std::unique_ptr<CStakeInput> > listInputs;
+        std::list <std::unique_ptr<CStakeInput>> listInputs;
         if (!SelectStakeCoins(listInputs, 0, true)) {
             MilliSleep(5000);
             continue;
@@ -5553,17 +5562,20 @@ void CWallet::PrecomputeSpends()
 
         // If we haven't loaded from database yet, load the precomputes from the database
         if (!fLoadedPrecomputesFromDB) {
-            CWalletDB walletdb("precomputes.dat", "cr+");
-            walletdb.ReadPrecomputes(mapPrecomputeCache);
+            // Load the precomputes into the LRU cache
+            walletdb.LoadPrecomputes(item_list, item_map);
             fLoadedPrecomputesFromDB = true;
-            LogPrint("precompute", "%s: Loaded precomputes from database. Size of map: %d\n", __func__, mapPrecomputeCache.size());
+            LogPrint("precompute", "%s: Loaded precomputes from database. Size of lru cache: %d\n", __func__,
+                     item_map.size());
         }
 
         // Do some precomputing of zerocoin spend knowledge proofs
-        std::set<uint256> setInputHashes;
-        for (std::unique_ptr<CStakeInput>& stakeInput : listInputs) {
+        std::set <uint256> setInputHashes;
+        for (std::unique_ptr <CStakeInput>& stakeInput : listInputs) {
             if (ShutdownRequested() || IsLocked())
                 break;
+
+            CoinWitnessCacheData tempDataHolder;
 
             {
                 TRY_LOCK(zhlmTracker->cs_spendcache, fLocked);
@@ -5575,10 +5587,10 @@ void CWallet::PrecomputeSpends()
                 if (fClearSpendCache) {
                     break;
                 }
-                
+
                 uint256 serialHash = stakeInput->GetSerialHash();
                 setInputHashes.insert(serialHash);
-                CoinWitnessData *witnessData = zhlmTracker->GetSpendCache(serialHash);
+                CoinWitnessData* witnessData = zhlmTracker->GetSpendCache(serialHash);
 
                 // Initialize nHeightStop so it can be set below
                 int nHeightStop = 0;
@@ -5586,10 +5598,14 @@ void CWallet::PrecomputeSpends()
                 if (witnessData->nHeightAccStart) { // Witness is already valid
                     nHeightStop = std::min(chainActive.Height() - nRequiredStakeDepthBuffer,
                                            (witnessData->nHeightAccEnd ? witnessData->nHeightAccEnd
-                                                                       : witnessData->nHeightAccStart) + nAdjustableCacheLength);
-                } else if (mapPrecomputeCache.count(serialHash)) { // Check Database cache
+                                                                       : witnessData->nHeightAccStart) +
+                                           nAdjustableCacheLength);
+                } else if (item_map.count(serialHash)) { // Check Database cache
                     // Get the witness data from the cache
-                    *witnessData = CoinWitnessData(mapPrecomputeCache.at(serialHash));
+                    auto it = item_map.find(serialHash);
+                    item_list.splice(item_list.begin(), item_list, it->second);
+
+                    *witnessData = CoinWitnessData(it->second->second);
 
                     // Set the stop height from the variables received from the database cache
                     nHeightStop = std::min(chainActive.Height() - nRequiredStakeDepthBuffer,
@@ -5597,66 +5613,148 @@ void CWallet::PrecomputeSpends()
                                                                        : witnessData->nHeightAccStart) +
                                            nAdjustableCacheLength);
 
-                    LogPrint("precompute", "%s: Got Witness Data from Database: %s\n", __func__, witnessData->ToString());
+                    LogPrint("precompute", "%s: Got Witness Data from lru cache: %s\n", __func__,
+                             witnessData->ToString());
+                } else if (mapDirtyWitnessData.count(serialHash) || walletdb.ReadPrecompute(serialHash, tempDataHolder)) {
+                    if (mapDirtyWitnessData.count(serialHash)) {
+                        // Get the witness data from the dirty cache if it exists
+                        *witnessData = CoinWitnessData(mapDirtyWitnessData.at(serialHash));
+                        LogPrint("precompute", "%s: Got Witness Data from mapDirtyWitnessData: %s\n", __func__,
+                                 witnessData->ToString());
+                    } else {
+                        // Get the witness data from the database
+                        *witnessData = CoinWitnessData(tempDataHolder);
+                        LogPrint("precompute", "%s: Got Witness Data from precompute database: %s\n", __func__,
+                                 witnessData->ToString());
+                    }
+
+                    // Set the stop height from the variables received from the database cache
+                    nHeightStop = std::min(chainActive.Height() - nRequiredStakeDepthBuffer,
+                                           (witnessData->nHeightAccEnd ? witnessData->nHeightAccEnd
+                                                                       : witnessData->nHeightAccStart) +
+                                           nAdjustableCacheLength);
+
+                    // Add the serialHash found into the cache
+                    item_list.push_front(make_pair(serialHash, tempDataHolder));
+                    item_map.insert(make_pair(serialHash, item_list.begin()));
+
+                    // We just added a new hash into our LRU cache, so remove it if we also have it in the dirty map
+                    mapDirtyWitnessData.erase(serialHash);
+
+                    if (item_map.size() > PRECOMPUTE_LRU_CACHE_SIZE) {
+                        auto last_it = item_list.end(); last_it --;
+                        item_map.erase(last_it->first);
+                        CoinWitnessCacheData removedData = item_list.back().second;
+                        mapDirtyWitnessData[serialHash] = removedData;
+                        item_list.pop_back();
+                    }
                 } else { // This has no cache, so initialize it
                     CZerocoinMint mint;
                     if (!GetMintFromStakeHash(serialHash, mint))
                         continue;
                     *witnessData = CoinWitnessData(mint);
-                    nHeightStop = std::min(chainActive.Height() - nRequiredStakeDepthBuffer, mint.GetHeight() + nAdjustableCacheLength);
+                    nHeightStop = std::min(chainActive.Height() - nRequiredStakeDepthBuffer,
+                                           mint.GetHeight() + nAdjustableCacheLength);
                 }
 
                 if (nHeightStop - (witnessData->nHeightAccEnd ? witnessData->nHeightAccEnd : witnessData->nHeightAccStart) < 20)
                     continue;
 
-                CBlockIndex *pindexStop = chainActive[nHeightStop];
+                CBlockIndex* pindexStop = chainActive[nHeightStop];
                 AccumulatorMap mapAccumulators(Params().Zerocoin_Params(false));
-                LogPrintf("%s: caching mint %s of denom %d start=%d stop=%d end=%s\n", __func__,
+                LogPrint("precompute", "%s: caching mint %s of denom %d start=%d stop=%d end=%s\n", __func__,
                           witnessData->coin->getValue().GetHex().substr(0, 6),
                           ZerocoinDenominationToInt(witnessData->denom),
                           witnessData->nHeightAccStart, nHeightStop, witnessData->nHeightAccEnd);
 
                 if (!GenerateAccumulatorWitness(witnessData, mapAccumulators, pindexStop)) {
-                    LogPrintf("%s: caching of witness failed!\n", __func__);
+                    LogPrintf("%s: Generate witness failed!\n", __func__);
 
-                    // If we fail this check, we need to make sure we remove this from the pre compute cache
-                    mapPrecomputeCache.erase(serialHash);
+                    // If we fail this check, we need to make sure we remove this from the LRU cache
+                    auto it = item_map.find(serialHash);
+                    if (it != item_map.end())
+                    {
+                        item_list.erase(it->second);
+                        item_map.erase(it);
+                    }
+                    mapDirtyWitnessData.erase(serialHash);
+                    walletdb.ErasePrecompute(serialHash);
                     continue;
                 }
 
-                // Update the database cache with the new witness data
+
                 CoinWitnessCacheData serialData(witnessData);
-                mapPrecomputeCache[serialHash] = serialData;
+
+                // If the LRU cache already has a entry for it, update the entry and move it to the front of the list
+                auto it = item_map.find(serialHash);
+                if (it != item_map.end()) {
+                    item_list.splice(item_list.begin(), item_list, it->second);
+                    item_list.begin()->second = serialData;
+                } else {
+                    item_list.push_front(make_pair(serialHash, serialData));
+                    item_map.insert(make_pair(serialHash, item_list.begin()));
+                }
+
+                // We just added a new hash into our LRU cache, so remove it if we also have it in the dirty map
+                mapDirtyWitnessData.erase(serialHash);
+
+                // Clean up the LRU cache to the max size
+                while (item_map.size() > PRECOMPUTE_LRU_CACHE_SIZE) {
+                    auto last_it = item_list.end(); last_it --;
+                    item_map.erase(last_it->first);
+                    mapDirtyWitnessData[serialHash] = item_list.back().second;
+                    item_list.pop_back();
+                }
             }
             // Sleep for 150ms to allow any potential spend attempt
             MilliSleep(150);
         }
 
         // On first load, and every 5 minutes clean up our cache with only valid unspent inputs
-        if (fOnFirstLoad || nLastCacheCleanUpTime < GetTime() - 300) {
+        if (fOnFirstLoad || nLastCacheCleanUpTime < GetTime() - PRECOMPUTE_FLUSH_TIME) {
             LogPrint("precompute", "%s: Cleaning up precompute cache\n", __func__);
 
             // We only want to clear the cache if we have calculated new witness data
-           if (setInputHashes.size()) {
-                // Create temporary cache to hold witness data
-                std::map<uint256, CoinWitnessCacheData> mapLatestCoinWitnessData;
+            if (setInputHashes.size()) {
+                // Get a list of hashes currently in the database
+                set <uint256> databaseHashes;
+                walletdb.LoadPrecomputes(databaseHashes);
 
                 // Remove old cache data
                 for (auto inputHash : setInputHashes) {
-                    if (mapPrecomputeCache.count(inputHash))
-                        mapLatestCoinWitnessData.insert(make_pair(inputHash, mapPrecomputeCache.at(inputHash)));
+                    databaseHashes.erase(inputHash);
                 }
-                mapPrecomputeCache = mapLatestCoinWitnessData;
+
+                // Erase all old hashes from the database
+                for (auto hash : databaseHashes) {
+                    auto it = item_map.find(hash);
+                    if (it != item_map.end())
+                    {
+                        item_list.erase(it->second);
+                        item_map.erase(it);
+                    }
+                    mapDirtyWitnessData.erase(hash);
+                    walletdb.ErasePrecompute(hash);
+                }
+
                 nLastCacheCleanUpTime = GetTime();
             }
         }
 
         // On first load, and every 5 minutes write the cache to database
-        if (fOnFirstLoad || nLastCacheWriteDB < GetTime() - 300 || ShutdownRequested()) {
-            CWalletDB walletdb("precomputes.dat", "cr+");
-            walletdb.WritePrecomputes(mapPrecomputeCache);
+        if (mapDirtyWitnessData.size() > PRECOMPUTE_MAX_DIRTY_CACHE_SIZE || nLastCacheWriteDB < GetTime() - PRECOMPUTE_FLUSH_TIME || ShutdownRequested()) {
+            // Save all cache data that was dirty back into the database
+            for (auto item : mapDirtyWitnessData) {
+                walletdb.WritePrecompute(item.first, item.second);
+            }
+            mapDirtyWitnessData.clear();
 
-            LogPrint("precompute", "%s: Writing precomputes to database. Precomputes size: %d\n", __func__, mapPrecomputeCache.size());
+            // Save the LRU cache data into the database
+            for (auto item : item_list) {
+                walletdb.WritePrecompute(item.first, item.second);
+            }
+
+            LogPrint("precompute", "%s: Writing precomputes to database. Precomputes size: %d\n", __func__, item_map.size());
             nLastCacheWriteDB = GetTime();
         }
 